56942dd814eaa0046e61d3507e73f5640eac6ae6
aoa9 <aoa9@cornell.edu>
2023-02-17 20:05:26 -0500

    latest schema

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -2,67 +2,22 @@ import { Prisma } from "@prisma/client";
 import prisma from "../client";
 
 const userData: Prisma.UserCreateInput[] = [
-  {
-    name: "Alice",
-    email: "alice@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Join the Prisma Slack",
-          content: "https://slack.prisma.io",
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: "Nilu",
-    email: "nilu@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Follow Prisma on Twitter",
-          content: "https://www.twitter.com/prisma",
-          published: true,
-        },
-      ],
-    },
-  },
-  {
-    name: "Mahmoud",
-    email: "mahmoud@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Ask a question about Prisma on GitHub",
-          content: "https://www.github.com/prisma/prisma/discussions",
-          published: true,
-        },
-        {
-          title: "Prisma on YouTube",
-          content: "https://pris.ly/youtube",
-        },
-      ],
-    },
-  },
-];
+
+]
 
 async function main() {
-  // This is for demo purposes only. Everytime we start the server, our seed script will run.
-  // But, we will get constraint errors becuause we are creating the same data again.
-
-  console.log("deleting previous seed data");
-  await prisma.post.deleteMany();
-  await prisma.user.deleteMany();
-  console.log(`Start seeding ...`);
+  console.log(`Start seeding ...`)
   for (const u of userData) {
     const user = await prisma.user.create({
       data: u,
-    });
-    console.log(`Created user with id: ${user.id}`);
+    })
+    console.log(`Created user with id: ${user.id}`)
   }
-  console.log(`Seeding finished.`);
+  console.log(`Seeding finished.`)
 }
+  // This is for demo purposes only. Everytime we start the server, our seed script will run.
+  // But, we will get constraint errors becuause we are creating the same data again.
+
 
 main()
   .then(async () => {
