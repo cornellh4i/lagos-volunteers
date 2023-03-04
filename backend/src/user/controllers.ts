a5dd830bfbe07b2405ea2acddd1c50fbb3741942
Daniel Botros <dab463@cornell.edu>
2023-03-04 16:00:29 -0500

    Created create, delete, and update user routes endpoints

--- backend/src/user/controllers.ts
+++ backend/src/user/controllers.ts
@@ -1,22 +1,91 @@
-import { Prisma } from "@prisma/client";
 import { Request, Response } from "express";
 import prisma from "../../client";
 
 /**
- * Creates a new user with all information specified in the JSON body 
- * @returns promise with userID or error
+ * Creates a new user with information specified in the request body.
+ * Request body includes:
+ * - Email (String)
+ * - Role (userRole) 
+ * - Profile (Profile)
+ * - Verified (Boolean)
+ * - Hours (Int)
+ * - Status (UserStatus)
+ * - Created Events (Event[])
+ * - Events (EventEnrollment[])
+ * - Preferences (UserPreferences)
+ * - Permissions (Permission[])
+ * @returns promise with user or error.
  */
 const createUser = async (req: Request, res: Response) => {
-  // #swagger.tags = ['Users']
   try {
-    const users = await prisma.user.findMany();
-    res.status(200).json(users);
+    const newUser = await prisma.user.create({
+      data: {
+        ...req.body,
+      },
+    })
+
+    res.status(200).json(newUser);
   } catch (error: any) {
-    // Do we need to log the error?
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/**
+ * Deletes specified user by userID.
+ * @returns promise with userID or error.
+ */
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const userID = req.params.userID
+
+    const deleteUser = await prisma.user.delete({
+      where: {
+        id: userID,
+      },
+    });
+    res.status(200).json(userID);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/**
+ * Updates a user with information specified in the request body. 
+ * Request body includes:
+ * - Email (String)
+ * - Role (userRole) 
+ * - Profile (Profile)
+ * - Verified (Boolean)
+ * - Hours (Int)
+ * - Status (UserStatus)
+ * - Created Events (Event[])
+ * - Events (EventEnrollment[])
+ * - Preferences (UserPreferences)
+ * - Permissions (Permission[])
+ * @returns promise with userID or error.
+ */
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    const userID = req.params.userID
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: userID
+      },
+      data: {
+        ...req.body,
+      },
+    })
+
+    res.status(200).json(userID);
+  } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
 };
 
 export default {
-  createUser
+  createUser,
+  deleteUser,
+  updateUser
+
 };
