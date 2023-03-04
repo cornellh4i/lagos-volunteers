fdae205b5c06adb13b1ce8897b8c01012d78b554
JJ Bai <jhb342@cornell.edu>
2023-03-04 16:02:06 -0500

    Finished testing event endpoints
    Co-authored-by: Yichen Yao <y.yichen@outlook.com>

--- backend/tests/test.ts
+++ backend/tests/test.ts
@@ -2,16 +2,24 @@ import request from "supertest";
 import app from "../src/index";
 import prisma from "../client";
 
-beforeAll(async () => {
-  await prisma.$connect();
-});
 
-afterAll(async () => {
-  await prisma.$disconnect();
-  await app.set('connection', 'close')
-});
 
-describe("Testing events Endpoints", () => {
+// beforeAll(async () => {
+//   await prisma.$connect();
+// });
+
+// afterAll(async () => {
+//   await prisma.$disconnect();
+//   await app.set('connection', 'close')
+// });
+
+describe("Testing Endpoints", () => {
+  test("Get all users in DB", async () => {
+    const response = await request(app).get("/users");
+    expect(response.status).toBe(200);
+  });
+
+
   test("Get all events in DB", async () => {
     const response = await request(app).get("/event/all");
     expect(response.status).toBe(200);
@@ -31,7 +39,13 @@ describe("Testing events Endpoints", () => {
     const response = await request(app).get("/event/past");
     expect(response.status).toBe(200);
   });
-
+  // test("Create a new user", async () => {
+  //   const user = {
+  //     email: "johnw@gmail.com",
+  //   };
+  //   const response = await request(app).post("/user/signup").send(user);
+  //   expect(response.status).toBe(201);
+  // });
 });
 
-
+// TODO: Add Tests for post endpoints
