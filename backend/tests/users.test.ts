7e25bd16736aaac37434d28c2565d6bd01df075e
Sophie Wang <sophwang024@gmail.com>
2023-03-06 22:05:14 -0500

    Added 2 Jest tests for each endpoint.

--- backend/tests/users.test.ts
+++ backend/tests/users.test.ts
@@ -26,6 +26,60 @@ describe("Testing user Endpoints", () => {
     const response = await request(app).post("/user/signup").send(user);
     expect(response.status).toBe(201);
   });
+
+
+  test("Create a new user", async () => {
+    const user = {
+      email: "alw284@cornell.edu",
+      profile: {
+        create: {
+          firstName: "Anya",
+          lastName: "Wang",
+          nickname: "Sheep",
+        }
+      },
+    };
+    const response = await request(app).post("/user/signup").send(user);
+    expect(response.status).toBe(201);
+  });
+
+  test("Fail to create a new user", async () => {
+    const user = {
+
+    };
+    const response = await request(app).post("/user/signup").send(user);
+    expect(response.status).toBe(500);
+  });
+
+  test("Successfully update user", async () => { //F
+    const user = {
+      email: "asu284@cornell.edu",
+    };
+    const userid = "clexnw7p60002hd5hlwaiyqvk"
+    const response = await request(app).put("/user/update/:" + userid).send(user);
+    expect(response.status).toBe(200);
+  });
+
+  test("Update user fail", async () => {
+    const user = {
+      email: "asu284@cornell.edu",
+    };
+    const userid = -1
+    const response = await request(app).put("/user/update/" + userid).send(user);
+    expect(response.status).toBe(404);
+  })
+
+  test("Delete valid user", async () => { //F
+    const userid = "clexnw7p60002hd5hlwaiyqvk";
+    const response = await request(app).delete("/user/delete/:" + userid);
+    expect(response.status).toBe(200);
+  });
+
+  test("Delete invalid user", async () => {
+    const userid = -1
+    const response = await request(app).delete("/user/delete/:" + userid);
+    expect(response.status).toBe(404);
+  });
 });
 
 // TODO: Add Tests for post endpoints
