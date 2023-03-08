31610676817e74784b2dd9818e801ba538e11504
Daniel Botros <dab463@cornell.edu>
2023-03-08 10:32:00 -0500

    Edits to fulfill PR suggestions

--- backend/src/users/controllers.ts
+++ backend/src/users/controllers.ts
@@ -41,7 +41,92 @@ const createNewUser = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Creates a new user with information specified in the request body.
+ * Request body includes:
+ * - Email (String)
+ * - Role (userRole) 
+ * - Profile (Profile)
+ * - Verified (Boolean)
+ * - Hours (Int)
+ * - Status (UserStatus)
+ * - Created Events (Event[])
+ * - Events (EventEnrollment[])
+ * - Preferences (UserPreferences)
+ * - Permissions (Permission[])
+ * @returns promise with user or error.
+ */
+const createUser = async (req: Request, res: Response) => {
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        ...req.body,
+      },
+    })
+
+    res.status(200).json(newUser);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/**
+ * Deletes specified user by userID.
+ * @returns promise with userID or error.
+ */
+const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const userID = req.params.userID
+
+    const deleteUser = await prisma.user.delete({
+      where: {
+        id: userID,
+      },
+    });
+    res.status(200).json(userID);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+/**
+ * Updates a user with information specified in the request body. 
+ * Request body includes:
+ * - Email (String)
+ * - Role (userRole) 
+ * - Profile (Profile)
+ * - Verified (Boolean)
+ * - Hours (Int)
+ * - Status (UserStatus)
+ * - Created Events (Event[])
+ * - Events (EventEnrollment[])
+ * - Preferences (UserPreferences)
+ * - Permissions (Permission[])
+ * @returns promise with userID or error.
+ */
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    const userID = req.params.userID
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: userID
+      },
+      data: {
+        ...req.body,
+      },
+    })
+
+    res.status(200).json(userID);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 export default {
   getUsers,
   createNewUser,
+  createUser,
+  deleteUser,
+  updateUser
 };
--- backend/src/users/views.ts
+++ backend/src/users/views.ts
@@ -8,5 +8,8 @@ const userRouter = Router();
 // This approach is cleaner for us becuase we can easily add middle ware
 userRouter.get("/", userController.getUsers);
 userRouter.post("/signup", userController.createNewUser);
+userRouter.post("/users", userController.createUser);
+userRouter.delete("/users/:userID", userController.deleteUser);
+userRouter.put("/users/:userID", userController.updateUser);
 
 export default userRouter;
