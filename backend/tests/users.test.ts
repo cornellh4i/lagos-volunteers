f38e4de80cf63043bc8edfee49cbb87676ca9dc2
Sophie Wang <sophwang024@gmail.com>
2023-03-10 05:41:50 -0500

    Tests follow the format in the PR Checklist Guide

--- backend/tests/users.test.ts
+++ backend/tests/users.test.ts
@@ -27,59 +27,64 @@ describe("Testing user Endpoints", () => {
     expect(response.status).toBe(201);
   });
 
+  describe ("Testing post /users", () => {
+    test("Create a new user", async () => {
+      const user = {
+        email: "alw284@cornell.edu",
+        profile: {
+          create: {
+            firstName: "Anya",
+            lastName: "Wang",
+            nickname: "Sheep",
+          }
+        },
+      };
+      const response = await request(app).post("/users").send(user);
+      expect(response.status).toBe(201);
+    });
 
-  test("Create a new user", async () => {
-    const user = {
-      email: "alw284@cornell.edu",
-      profile: {
-        create: {
-          firstName: "Anya",
-          lastName: "Wang",
-          nickname: "Sheep",
-        }
-      },
-    };
-    const response = await request(app).post("/user/signup").send(user);
-    expect(response.status).toBe(201);
-  });
 
-  test("Fail to create a new user", async () => {
-    const user = {
+    test("Fail to create a new user", async () => {
+      const user = {
 
-    };
-    const response = await request(app).post("/user/signup").send(user);
-    expect(response.status).toBe(500);
-  });
+      };
+      const response = await request(app).post("/users").send(user);
+      expect(response.status).toBe(500);
+    });
+}); 
 
-  test("Successfully update user", async () => { //F
-    const user = {
-      email: "asu284@cornell.edu",
-    };
-    const userid = "clexnw7p60002hd5hlwaiyqvk"
-    const response = await request(app).put("/user/update/:" + userid).send(user);
-    expect(response.status).toBe(200);
-  });
+  describe ("Testing put /users/:userid", () => {
+    test("Successfully update user", async () => { 
+      const user = {
+        email: "asu284@cornell.edu",
+      };
+      const userid = "clexnw7p60002hd5hlwaiyqvk"
+      const response = await request(app).put("/users/:" + userid).send(user);
+      expect(response.status).toBe(200);
+    });
 
-  test("Update user fail", async () => {
-    const user = {
-      email: "asu284@cornell.edu",
-    };
-    const userid = -1
-    const response = await request(app).put("/user/update/" + userid).send(user);
-    expect(response.status).toBe(404);
-  })
+    test("Update user fail", async () => {
+      const user = {
+        email: "asu284@cornell.edu",
+      };
+      const userid = -1
+      const response = await request(app).put("/users/:" + userid).send(user);
+      expect(response.status).toBe(404);
+    })
+  }); 
 
-  test("Delete valid user", async () => { //F
-    const userid = "clexnw7p60002hd5hlwaiyqvk";
-    const response = await request(app).delete("/user/delete/:" + userid);
-    expect(response.status).toBe(200);
-  });
+  describe ("Testing delete /user/delete/:",() => { 
+    test("Delete valid user", async () => { 
+      const userid = "clexnw7p60002hd5hlwaiyqvk";
+      const response = await request(app).delete("/users/:" + userid);
+      expect(response.status).toBe(200);
+    });
 
-  test("Delete invalid user", async () => {
-    const userid = -1
-    const response = await request(app).delete("/user/delete/:" + userid);
-    expect(response.status).toBe(404);
-  });
+    test("Delete invalid user", async () => {
+      const userid = -1
+      const response = await request(app).delete("/users/:" + userid);
+      expect(response.status).toBe(404);
+    });}); 
 });
 
 // TODO: Add Tests for post endpoints
