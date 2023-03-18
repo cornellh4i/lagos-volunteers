91b4f5ff9db04ef8e018559ce0047db781b3c7ac
Sneha Rajaraman <sneha_rajaraman@dhcp-vl2042-6963.redrover.cornell.edu>
2023-03-18 16:29:02 -0400

    sample

--- backend/src/events/views.ts
+++ backend/src/events/views.ts
@@ -16,5 +16,6 @@ eventRouter.get("/", eventController.getEvents);
 eventRouter.get("/upcoming", eventController.getUpcomingEvents);
 eventRouter.get("/current", eventController.getCurrentEvents);
 eventRouter.get("/past", eventController.getPastEvents);
+eventRouter.delete("/events/:eventid/attendees/:attendeeid", eventController.deleteAttendee);
 
 export default eventRouter;
