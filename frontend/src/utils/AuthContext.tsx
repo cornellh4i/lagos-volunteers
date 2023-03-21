6267f908a02ad61e6117a1da9f308b93c8732cda
aoa9 <aoa9@cornell.edu>
2023-03-21 16:29:44 -0400

    frontend-base setup

---
+++ frontend/src/utils/AuthContext.tsx
@@ -0,0 +1,80 @@
+import React, {
+	createContext,
+	useState,
+	useContext,
+	useEffect,
+	ReactNode,
+} from 'react';
+import { auth } from './firebase';
+import { User, AuthError, signOut, signInWithCustomToken } from 'firebase/auth';
+import {
+	useAuthState,
+	useSignInWithEmailAndPassword,
+} from 'react-firebase-hooks/auth';
+
+// Define types for authentication context value
+type AuthContextValue = {
+	user: User | null | undefined;
+	loading: boolean;
+	error?: AuthError | Error | null | undefined;
+	signInUser: (email: string, password: string) => Promise<void>;
+	signOutUser: () => Promise<void>;
+	signInUserWithCustomToken: (token: string) => Promise<void>;
+};
+
+export const AuthContext = createContext<AuthContextValue>({
+	user: undefined,
+	loading: true,
+	signInUser: async () => {},
+	signInUserWithCustomToken: async () => {},
+	signOutUser: async () => {},
+});
+
+// Define props type for authentication provider
+type AuthProviderProps = {
+	children: ReactNode;
+};
+
+export const useAuth = () => {
+	return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+	const [user, loading, error] = useAuthState(auth);
+	const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+
+	const signInUserWithCustomToken = async (token: string) => {
+		try {
+			const result = await signInWithCustomToken(auth, token);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const signInUser = async (email: string, password: string) => {
+		try {
+			const result = await signInWithEmailAndPassword(email, password);
+		} catch (error) {
+			// TODO: Handle Different Auth Errors
+			console.log(error);
+		}
+	};
+
+	const signOutUser = async () => {
+		try {
+			await signOut(auth);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	const value = {
+		user,
+		loading,
+		error,
+		signInUser,
+		signOutUser,
+		signInUserWithCustomToken,
+	};
+
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	corePlugins: {
+		preflight: false,
+	},
+	important: '#__next',
 	theme: {
 		extend: {},
 	},
