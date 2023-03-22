7a37483883d9e3c54f09c6029748fc1dff0e2ca3
jasozh <48730262+jasozh@users.noreply.github.com>
2023-03-22 17:18:44 -0500

    Frontend onboarding setup (#35)
    * Fix frontend dotenv issues
    
    * Format frontend files using Prettier
    
    * Change login.tsx to Login.tsx
    
    * Change function format to const

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,39 +1,41 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
-import { StyledEngineProvider } from '@mui/material/styles';
-import Layout from '@/components/Layout';
-import { AuthProvider } from '@/utils/AuthContext';
-import { useAuth } from '@/utils/AuthContext';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { StyledEngineProvider } from "@mui/material/styles";
+import Layout from "@/components/Layout";
+import { AuthProvider } from "@/utils/AuthContext";
+import { useAuth } from "@/utils/AuthContext";
 
 export const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#556cd6',
-		},
-		secondary: {
-			main: '#19857b',
-		},
-		error: {
-			main: '#ff0000',
-		},
-		background: {
-			default: '#fff',
-		},
-	},
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: "#ff0000",
+    },
+    background: {
+      default: "#fff",
+    },
+  },
 });
 
-export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<StyledEngineProvider injectFirst>
-			<ThemeProvider theme={theme}>
-				<Layout>
-					<AuthProvider>
-						<Component {...pageProps} />
-					</AuthProvider>
-				</Layout>
-			</ThemeProvider>
-		</StyledEngineProvider>
-	);
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <AuthProvider>
+            <Component {...pageProps} />
+          </AuthProvider>
+        </Layout>
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+};
+
+export default App;
