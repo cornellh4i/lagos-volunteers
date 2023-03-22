1134febf3bce7aab5231fc2c21b422057d4ae124
Jason Zheng <jasonz4200@gmail.com>
2023-03-22 13:56:10 -0400

    Format frontend files using Prettier

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{js,ts,jsx,tsx}'],
-	corePlugins: {
-		preflight: false,
-	},
-	important: '#__next',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  corePlugins: {
+    preflight: false,
+  },
+  important: "#__next",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
