b3a2ed5534915759fa5c4690df15a417b6c37515
Jason Zheng <jasonz4200@gmail.com>
2023-03-22 13:51:10 -0400

    Fix frontend dotenv issues

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -1,7 +1,16 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getAnalytics } from 'firebase/analytics';
-import { firebaseConfig } from '@/config/firebaseconfig';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
