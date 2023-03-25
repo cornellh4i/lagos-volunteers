6997c396c342c66f6b8d83656b1cc5ec9dfa091a
Yichen Yao <yy486@cornell.edu>
2023-03-24 22:21:38 -0400

    Complete cat api call

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -5,7 +5,21 @@ import { useRouter } from "next/router";
 const User = () => {
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+  return <>
+  Hello user {userid}!
+  <button onClick={handleClick}>cat fact</button>
+  </>;
+  
+};
+
+const getCatFact = async () => {
+  const response = await fetch("https://catfact.ninja/fact");
+  const fact = await response.json()
+  console.log(fact);
+};
+
+const handleClick = () => {
+  getCatFact();
 };
 
 export default User;
