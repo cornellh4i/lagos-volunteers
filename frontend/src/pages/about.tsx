ae27ba24e4b8678712aed4efe863b752d2724989
JJ Bai <jhb342@cornell.edu>
2023-03-25 15:15:46 -0400

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -10,6 +10,13 @@ const About = () => {
       </div>
       <Button text="hello" />
       Hello there
+      <div className="container mx-auto py-20">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 content-center">
+          <div className="h-40 col-span-2 rounded-xl p-6 bg-red-400"></div>
+          <div className="h-40 col-span-2 rounded-xl p-6 bg-blue-400"></div>
+          <div className="h-40 col-span-2 lg:col-span-4 rounded-xl p-6 bg-green-400"></div>
+        </div>
+      </div>
     </>
   );
 };
