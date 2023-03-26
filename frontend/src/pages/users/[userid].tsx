fafef4bc0cc23f365d18ca2a8e06f5112a15bf1a
Daniel Botros <dab463@cornell.edu>
2023-03-26 14:18:02 -0400

    A cat’s eyesight is both better and worse than hum…do. Scientists believe grass appears red to cats.'

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { useRouter } from "next/router";
 
+const getCatFact = async () => {
+  const response = await fetch("https://catfact.ninja/fact");
+  const fact = await response.json();
+  console.log(fact);
+};
+
+const handleClick = () => {
+  getCatFact();
+};
+
 /** A User page */
 const User = () => {
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+  return <>
+  Hello user {userid}
+  <button onClick={handleClick}>cat fact</button> 
+  </>;
+
 };
 
 export default User;
