0011fb3f70a7ceee58a85c6ecb668b85b8258286
Daniel Botros <dab463@cornell.edu>
2023-03-26 14:59:17 -0400

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -6,8 +6,18 @@ const About = () => {
   return (
     <>
       <div className="bg-yellow-300 bg-opacity-10 w-1/2 tracking-tighter">Hello</div>
+
       <Button text="Hello"/>
       Hello there
+      
+      <div className="container mx-auto py-20">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 content-center">
+          <div className="h-40 col-span-2 rounded-xl p-6 bg-red-500"></div>
+          <div className="h-40 col-span-2 rounded-xl p-6 bg-blue-500"></div>
+          <div className="h-40 col-span-2 lg:col-span-4 rounded-xl p-6 bg-green-500"></div>
+        </div>
+      </div>
+
     </>
   );
 };
