9ee90d2980777ee9bc9c9e08230e34e8ba49cc01
Sophie Wang <sophwang024@gmail.com>
2023-03-28 19:15:27 -0400

    Complete components onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from "@/components/Button";
 
+<div className="bg-black">Hello</div>
 /** An About page */
 const About = () => {
   return (
     <>
      <Button text="hello" />
+     <div className="bg-black bg-opacity-10 bg-yellow-300 w-0.5 tracking-tighter">Hello</div>
     </>
   );
 };
