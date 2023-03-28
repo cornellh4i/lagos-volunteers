c3397c44c54f2f61978526e5dde43ed8e864cc15
Sneha Rajaraman <sneha_rajaraman@dhcp-vl2042-6963.redrover.cornell.edu>
2023-03-28 17:35:24 -0400

    Cats can jump up to 7 times their tail length.

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -6,6 +6,17 @@ const User = () => {
   const router = useRouter();
   const { userid } = router.query;
   return <>Hello user {userid}!</>;
-};
+
+const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(response.json());
+  };
+  
+  const handleClick = () => {
+    getCatFact();
+  };
+
+  return <button onClick={handleClick}>cat fact</button>;
+}
 
 export default User;
