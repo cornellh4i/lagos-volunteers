c9f3e2c48b969b3479f90e4a51c93a62b88926a8
bananahz <hrz8@cornell.edu>
2023-03-28 01:16:27 -0400

    code cleanup

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -4,20 +4,18 @@ import { useRouter } from "next/router";
 /** A User Page */
 const User = () => {
   const router = useRouter();
-  const { userid } = router.query; 
-
 
   const getCatFact = async () => {
     const response = await fetch("https://catfact.ninja/fact");
     const catFact = await response.json();
     console.log(catFact);
   };
-  
+
   const handleClick = () => {
     getCatFact();
   };
 
   return <button onClick={handleClick}>cat fact</button>;
-}
+};
 
 export default User;
