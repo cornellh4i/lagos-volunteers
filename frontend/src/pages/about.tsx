abc2257f5a7a7614599c2c60863d50721bad7d31
Sophie Wang <sophwang024@gmail.com>
2023-03-28 19:34:11 -0400

    Complete responsive design onboarding.

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -6,8 +6,15 @@ import Button from "@/components/Button";
 const About = () => {
   return (
     <>
-     <Button text="hello" />
-     <div className="bg-black bg-opacity-10 bg-yellow-300 w-0.5 tracking-tighter">Hello</div>
+    <Button text="hello" />
+     {/*<div className="bg-black bg-opacity-10 bg-yellow-300 w-0.5 tracking-tighter">Hello</div>*/}
+     <div className="container mx-auto py-20">
+        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6 content-center">
+          <div className="h-40 col-span-2 rounded-xl p-6 bg-red-400"></div>
+          <div className="h-40 col-span-2 rounded-xl p-6 bg-blue-400"></div>
+          <div className="h-40 col-span-2 lg:col-span-4 rounded-xl p-6 bg-green-400"></div>
+        </div>
+      </div>
     </>
   );
 };
