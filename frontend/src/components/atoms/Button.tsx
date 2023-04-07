285580fcd8a85114fe08deae10ac5d6a77e9ffb3
bananahz <hrz8@cornell.edu>
2023-04-06 21:30:00 -0500

    finished base button minus font

--- frontend/src/components/atoms/Button.tsx
+++ frontend/src/components/atoms/Button.tsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { Button } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+interface Props {
+  buttonText: string;
+  buttonTextColor: string;
+  buttonColor: string;
+}
+
+const theme = createTheme({
+  // palette: {
+  //   primary: {
+  //     main: "#8D8D8D",
+  //   },
+  //   secondary: {
+  //     main: "#D9D9D9",
+  //   },
+  // },
+});
 /** A Button page */
-const Button = () => {
-  console.log("here");
-  return <>Hello there</>;
+const CustomButton = (props: Props) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Button
+        variant="contained"
+        style={{
+          backgroundColor: props.buttonColor,
+          fontFamily: "Inter", //NEED TO CHANGE, FONT AINT RIGHT D;
+          color: props.buttonTextColor,
+        }}
+      >
+        {props.buttonText}
+      </Button>
+    </ThemeProvider>
+  );
 };
 
-export default Button;
+export default CustomButton;
--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import CustomButton from "@/components/atoms/Button";
 
 /** A Signup page */
 const Signup = () => {
-  return <>Hello there</>;
+  return (
+    <>
+      <CustomButton
+        buttonText="Continue"
+        buttonColor="#8D8D8D"
+        buttonTextColor="#000000"
+      />
+      <CustomButton
+        buttonText="Continue with Google"
+        buttonColor="#D9D9D9"
+        buttonTextColor="#000000"
+      />
+    </>
+  );
 };
 
 export default Signup;
