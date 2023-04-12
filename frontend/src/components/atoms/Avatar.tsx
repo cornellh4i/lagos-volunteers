7000e7b2f6ffa7cf39a2b82dc886a8c6c02c4708
Yichen Yao <yy486@cornell.edu>
2023-04-11 21:01:53 -0400

    Implemented responsiveness with grid
    Co-authored-by: JJ <JJBai2003@users.noreply.github.com>

--- frontend/src/components/atoms/Avatar.tsx
+++ frontend/src/components/atoms/Avatar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import {useState} from "react";
 // import { Avatar } from "@mui/material";
 
-/** default royalty free avatarimage */
+/** default royalty free avatar image */
 export const FALLBACK_AVATAR_URL = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
 export const FALLBACK_AVATOR_ALT = "avatar"
 
@@ -15,18 +15,29 @@ type AvatarProps = {
   hour: number;
 }
 
-function Avatar ({url=FALLBACK_AVATAR_URL, alt=FALLBACK_AVATOR_ALT, name, start_date, hour}: AvatarProps) {
+const Avatar = ({url=FALLBACK_AVATAR_URL, alt=FALLBACK_AVATOR_ALT, name, start_date, hour}: AvatarProps) => {
   const [avatarSrc, setAvatarSrc] = useState(url)
-  return <>
-  <div className="flex flex-row">
-    <img src={url} alt={alt} className="rounded-full w-32" onError={()=>setAvatarSrc(FALLBACK_AVATAR_URL)}/>
-    <div className = "p-6 font-sans">
-      <div className="text-3xl font-semibold">{name}</div>
-      <div className = "py-1">Volunteer since {start_date.toLocaleDateString('en-US')}</div>
-      <div>{hour} volunteer hours completed</div>
-    </div>
-  </div>
-  </>
+  return (
+    <>
+      <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-12 content-center">
+        <div className="font-sans col-span-2 place-self-center">
+          <img
+            src={url}
+            alt={alt}
+            className="rounded-full w-32"
+            onError={() => setAvatarSrc(FALLBACK_AVATAR_URL)}
+          />
+        </div>
+        <div className="p-6 font-sans col-span-10 text-center lg:text-left">
+          <div className="text-3xl font-semibold">{name}</div>
+          <div className="py-1">
+            Volunteer since {start_date.toLocaleDateString("en-US")}
+          </div>
+          <div>{hour} volunteer hours completed</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Avatar;
