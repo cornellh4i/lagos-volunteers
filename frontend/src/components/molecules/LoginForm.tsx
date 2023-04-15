ec8273d8708e75b47a603a6f9ab15f2cc8fc6c72
Daniel Botros <dab463@cornell.edu>
2023-04-15 15:13:29 -0400

    basic login form

--- frontend/src/components/molecules/LoginForm.tsx
+++ frontend/src/components/molecules/LoginForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useAuth } from "@/utils/AuthContext";
+import Divider from '@mui/material/Divider';
 import Button from "../atoms/Button";
 import TextField from "../atoms/TextField";
 
@@ -38,8 +39,24 @@ const LoginForm = () => {
           buttonTextColor="#000000"
           buttonColor="#808080"/>
       </div>
+      <div>
+      <Divider>
+        or
+      </Divider>
+      </div>
+      <div>
+        <Button 
+          buttonText="Sign up with Email"
+          buttonTextColor="#000000"
+          buttonColor="#D3D3D3"/>
+      </div>
+      <div>
+        <Button 
+          buttonText="Continue with Google"
+          buttonTextColor="#000000"
+          buttonColor="#D3D3D3"/>
+      </div>
     </div>
   );
 };
-
 export default LoginForm;
