379b130e43b7e98dec1d03f59c4ec555fe1a4c39
Yichen Yao <yy486@cornell.edu>
2023-04-15 16:36:15 -0400

    form passed in as a prop

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ProfileTemplate from "../components/templates/ProfileTemplate"
+import ProfileForm from "@/components/molecules/ProfileForm";
 
 /** A Profile page */
 const Profile = () => {
   const date: Date = new Date();
-  return <ProfileTemplate name="Jason Zheng" start_date={date} hour={20}/>
+  return <ProfileTemplate name="Jason Zheng" start_date={date} hour={20} form={<ProfileForm/>}/>
 };
 
 export default Profile;
