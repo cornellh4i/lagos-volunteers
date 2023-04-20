7961981bbbce4ff25c9da99b91de897a0e39cde8
aoa9 <aoa9@cornell.edu>
2023-04-20 17:27:30 -0400

    add next router to reset page

--- frontend/src/pages/password/reset/[link].tsx
+++ frontend/src/pages/password/reset/[link].tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { useRouter } from "next/router";
+
 
 /** A ResetPassword page */
 const ResetPassword = () => {
-  return <>Hello there</>;
+
+    const router = useRouter();
+  const { link } = router.query;
+  return <>Hello there, {link}</>;
 };
 
 export default ResetPassword;
