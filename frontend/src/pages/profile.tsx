da06a90d98ea8c241406da199e1cf78eb56ed79f
aoa9 <aoa9@cornell.edu>
2023-04-21 17:10:22 -0400

    Fix profile form and template issues

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import ProfileForm from "@/components/molecules/ProfileForm";
+import React from 'react';
+import ProfileForm from '@/components/molecules/ProfileForm';
+import ProfileTemplate from '@/components/templates/ProfileTemplate';
 
 /** A Profile page */
 const Profile = () => {
-  return <ProfileForm />;
+	const date = new Date();
+	return (
+		<ProfileTemplate
+			name='Jason Zheng'
+			form={ProfileForm}
+			hour={20}
+			start_date={date}
+		/>
+	);
 };
 
 export default Profile;
