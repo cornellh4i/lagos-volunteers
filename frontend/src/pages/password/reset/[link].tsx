4e5b23d29201a727af8b2f6dba1bad0e8ca68a8b
Yichen Yao <yy486@cornell.edu>
2023-04-22 15:47:33 -0400

    complete frontend forgot password and reset form
    Co-authored-by: JJ <JJBai2003@users.noreply.github.com>

--- frontend/src/pages/password/reset/[link].tsx
+++ frontend/src/pages/password/reset/[link].tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useRouter } from "next/router";
+import ResetPasswordForm from "@/components/molecules/ResetPasswordForm";
 
 
 /** A ResetPassword page */
 const ResetPassword = () => {
 
-    const router = useRouter();
-  const { link } = router.query;
-  return <>Hello there, {link}</>;
+  //   const router = useRouter();
+  // const { link } = router.query;
+  return <ResetPasswordForm/>;
 };
 
 export default ResetPassword;
