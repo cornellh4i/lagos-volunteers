049977f1ebbf23f7fc466a41505745b7a88dc4c5
bananahz <hrz8@cornell.edu>
2023-04-22 17:48:55 -0400

    fix upload button styling

--- frontend/src/components/atoms/TextCopy.tsx
+++ frontend/src/components/atoms/TextCopy.tsx
@@ -1,33 +1,31 @@
-import React, {useState} from "react";
-import { Button, Snackbar } from '@mui/material'
-import ContentCopyIcon from '@mui/icons-material/ContentCopy';
-import IconButton from '@mui/material/IconButton';
+import React, { useState } from "react";
+import { Button, Snackbar } from "@mui/material";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import IconButton from "@mui/material/IconButton";
 
 interface Props {
-  label: string
-  text: string
+  label: string;
+  text: string;
 }
 
 /**
  * A TextCopy component has a label and a text body, along with a copy button.
  * Pressing the button copies the text body to the clipboard.
  */
-const TextCopy = ({label, text}: Props) => {
-  const [open, setOpen] = useState(false)
+const TextCopy = ({ label, text }: Props) => {
+  const [open, setOpen] = useState(false);
   const handleClick = () => {
-    setOpen(true)
-    navigator.clipboard.writeText(text)
-  }
-  
+    setOpen(true);
+    navigator.clipboard.writeText(text);
+  };
+
   return (
     <div>
-      <div className="flex space-x-64">
-        <div className="pt-3">
-          {label}:
-        </div>
-        <div>
+      <div className="flex ">
+        <div className="pt-3">{label}:</div>
+        <div className="absolute right-0">
           <IconButton onClick={handleClick}>
-            <ContentCopyIcon/>
+            <ContentCopyIcon />
           </IconButton>
           <Snackbar
             open={open}
@@ -36,13 +34,10 @@ const TextCopy = ({label, text}: Props) => {
             message="Copied to clipboard"
           />
         </div>
-     </div>
-     <div>
-        {text}
-     </div>
-  </div>
-
-  )
+      </div>
+      <div>{text}</div>
+    </div>
+  );
 };
 
 export default TextCopy;
