78e46b46bf73a03c6bed741c35850f42111f6722
bananahz <hrz8@cornell.edu>
2023-04-21 20:46:57 -0400

    date picker base code

--- frontend/src/components/atoms/DatePicker.tsx
+++ frontend/src/components/atoms/DatePicker.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 
+interface Props {
+  label: string;
+}
+
 /**
  * A DatePicker component is an input field that allows selecting a specific
  * date through a calendar popup
  */
-const DatePicker = () => {
-  return <>Date Picker</>;
+const DatePicker = ({ label }: Props) => {
+  return (
+    <div>
+      <div> {label} </div>
+      <DatePicker label={""} />
+    </div>
+  );
 };
 
 export default DatePicker;
--- frontend/src/components/molecules/EventForm.tsx
+++ frontend/src/components/molecules/EventForm.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import DatePicker from "../atoms/DatePicker";
 
 /** An EventForm page */
 const EventForm = () => {
-  return <>Hello there</>;
+  return (
+    <div>
+      <DatePicker label="Start Date" />
+      <DatePicker label="End Date" />
+    </div>
+  );
 };
 
 export default EventForm;
