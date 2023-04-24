07aff49748f62733f9eb81297324343d88ee8b5c
bananahz <hrz8@cornell.edu>
2023-04-24 13:25:33 -0400

    code factor

--- frontend/src/pages/events/[eventid]/edit.tsx
+++ frontend/src/pages/events/[eventid]/edit.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import EventForm from "@/components/molecules/EventForm";
 
 /** An EditEvent page */
 const EditEvent = () => {
-  const router = useRouter();
+  //const router = useRouter();
+  //const { eventid } = router.query;
   return <EventForm eventType="edit" />;
 };
 
