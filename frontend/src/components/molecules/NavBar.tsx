8a29e96d40f2f7715040cefc86291e186042dabd
Sneha Rajaraman <sneha_rajaraman@dhcp-vl2042-21666.redrover.cornell.edu>
2023-04-25 18:50:43 -0400

    Welcome template with Navbar

--- frontend/src/components/molecules/NavBar.tsx
+++ frontend/src/components/molecules/NavBar.tsx
@@ -12,20 +12,49 @@ export default function ButtonAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            LFBI
+          </Typography>
+
           <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-          >
+          >      
+            <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            LFBI
-          </Typography>
-          <MenuIcon />
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
+
+// import React from "react";
+// import { useState } from "react";
+
+// /** A NavBar page */
+// const NavBar = () => {
+//   const [menuOpen, setMenuOpen] = useState(false)
+
+//   const handleNav = () => {
+//     setMenuOpen(!menuOpen);
+//   }
+//   return (
+//   <>
+  
+//   <div className = "grid grid-cols-12">
+//     <div className = "col-span-11 justify-between items-center px-8 py-6 bg-gray-100 font-mono text-4xl">LFBI
+//     </div>
+//     <div className = "space-y-2 bg-gray-100 object-center py-8">
+//       <div className = "sm:w-0 w-8 h-0.5 bg-black animate-pulse"></div>
+//       <div className = "sm:w-0 w-8 h-0.5 bg-black animate-pulse"></div>
+//       <div className = "sm:w-0 w-8 h-0.5 bg-black animate-pulse"></div>
+//     </div>
+//   </div>
+//   </>
+//   );
+// };
+
+// export default NavBar;
