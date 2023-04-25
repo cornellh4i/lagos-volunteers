bf927c375e5a9977d82e827468d1c617d62dad89
Sophie Wang <sophwang024@gmail.com>
2023-04-25 19:08:06 -0400

    Added a handleClick to button

--- frontend/src/components/molecules/LoginForm.tsx
+++ frontend/src/components/molecules/LoginForm.tsx
@@ -1,67 +1,79 @@
-import React from "react";
-// import { useAuth } from "@/utils/AuthContext";
+import React, { useState } from "react";
+import { useAuth } from "@/utils/AuthContext";
 import Divider from "@mui/material/Divider";
 import Button from "../atoms/Button";
 import TextField from "../atoms/TextField";
+import Link from "next/link"
+import { auth } from '@/utils/firebase';
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const LoginForm = () => {
-  //const { signInUser } = useAuth();
 
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
+  const { signInUser } = useAuth();
 
-  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-  //   e.preventDefault();
-  //   signInUser(email, password);
-  // };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    signInUser(email, password);
+  };
 
   return (
-    <div className="">
+    <form className="">
       <div className="space-y-4 ">
         <div className="font-bold text-3xl"> Log In </div>
         <div>
           <TextField
-            label="Email"
+            handleChange={(e) => { setEmail(e.target.value); } }
             required={true}
             status=""
-            incorrectEntryText=""
-          />
+            incorrectEntryText="" label={""}          />
         </div>
         <div>
           <TextField
             label="Password"
             required={true}
             status=""
-            incorrectEntryText=""
-          />
+            incorrectEntryText="" handleChange={function (e: any): void {
+              throw new Error("Incorrect Entry");
+            } }          />
         </div>
-        <div className="text-center underline">Forgot Password?</div>
-        <div>
-          <Button
+        <div className="text-center underline"><Link href = "/password/forgot">Forgot Password?</Link></div>
+
+        <div className="pointer-events-auto">
+          <Link href = "/index">
+          <Button 
+            handleClick={handleSubmit}
             buttonText="Log In"
             buttonTextColor="#000000"
             buttonColor="#808080"
           />
+          </Link>
         </div>
         <div>
           <Divider>or</Divider>
         </div>
         <div>
+          <Link href = "/signup">
           <Button
+            handleClick={()=>console.log('signup')}
             buttonText="Sign up with Email"
             buttonTextColor="#000000"
             buttonColor="#D3D3D3"
           />
+          </Link>
         </div>
         <div>
           <Button
+          handleClick={()=>console.log('continue with google')}
             buttonText="Continue with Google"
             buttonTextColor="#000000"
             buttonColor="#D3D3D3"
           />
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
