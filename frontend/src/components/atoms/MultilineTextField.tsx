fdb8b736433f60ca38f606a645fbc80d4fa039f8
aoa9 <aoa9@cornell.edu>
2023-04-30 00:18:33 -0400

    eventform validation

--- frontend/src/components/atoms/MultilineTextField.tsx
+++ frontend/src/components/atoms/MultilineTextField.tsx
@@ -1,35 +1,45 @@
-import React from "react";
-import { TextField } from "@mui/material";
+import React from 'react';
+import { TextField } from '@mui/material';
+import { RegisterOptions, UseFormRegisterReturn } from 'react-hook-form';
 
 interface Props {
-  label: string;
-  required: boolean;
-  status: string;
-  incorrectEntryText: string;
+	label: string;
+	required: boolean;
+	type?: string;
+	requiredMessage?: string;
+	name: string;
+	register: (name: any, options?: RegisterOptions) => UseFormRegisterReturn;
 }
 
 /** A Button page */
 const MultilineTextField = ({
-  label,
-  required,
-  status,
-  incorrectEntryText,
+	label,
+	name,
+	required,
+	type = 'text',
+	requiredMessage = '',
+	register,
 }: Props) => {
-  return (
-    <div>
-      <div> {label} </div>
-      <TextField
-          id="outlined-multiline-static"
-          multiline
-          rows={4}
-          sx={{ borderRadius: 2, borderColor: "primary.main" }}
-          required={required}
-          fullWidth={true}
-          margin="dense"
-          helperText={status == "error" ? incorrectEntryText : ""}
-        />
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				{' '}
+				{label} <span className='text-red-500'>{requiredMessage}</span>
+			</div>
+			<TextField
+				id='outlined-multiline-static'
+				multiline
+				rows={4}
+				sx={{ borderRadius: 2, borderColor: 'primary.main' }}
+				required={required}
+				fullWidth={true}
+				{...register(name, {
+					required: required,
+				})}
+				margin='dense'
+			/>
+		</div>
+	);
 };
 
 export default MultilineTextField;
