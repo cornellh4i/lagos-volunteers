353da6720f80b8a608703ddb3b85f4ac2fc717ec
aoa9 <aoa9@cornell.edu>
2023-04-30 00:32:59 -0400

    resetPasswordForm

--- frontend/src/components/molecules/ResetPasswordForm.tsx
+++ frontend/src/components/molecules/ResetPasswordForm.tsx
@@ -1,49 +1,74 @@
-import React from "react";
-import Button from "../atoms/Button";
-import TextField from "../atoms/TextField";
-import Link from "next/link";
+import React from 'react';
+import Button from '../atoms/Button';
+import TextField from '../atoms/TextField';
+import Link from 'next/link';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type FormValues = {
+	password: string;
+	confirmPassword: string;
+};
 
 /** A ResetPassword page */
 const ResetPassword = () => {
-  return (
-    <div className="space-y-4">
-      <div className="font-bold text-3xl">Reset Password</div>
-      <div className="text-sm">
-        Password should meet the following requirements:
-        <ul className="m-0 px-4">
-          <li>At least 8 characters in length</li>
-          <li>Contains a mix of uppercase and lowercase letters</li>
-          <li>Includes at least one number and one special character</li>
-        </ul>
-      </div>
-      <div>
-        <TextField
-          label="Password*"
-          required={true}
-          status=""
-          incorrectEntryText=""
-        />
-      </div>
-      <div>
-        <TextField
-          label="Confirm Password*"
-          required={true}
-          status=""
-          incorrectEntryText=""
-        />
-      </div>
-      <div>
-        <Button
-          buttonText="Reset Password"
-          buttonTextColor="#000000"
-          buttonColor="#808080"
-        />
-      </div>
-      <div className="justify-center flex flex-row text-sm">
-        <Link href="/"> Didn't request to reset password?</Link>
-      </div>
-    </div>
-  );
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<FormValues>();
+
+	const handleSubmitForm: SubmitHandler<FormValues> = async (data) => {
+		const { password, confirmPassword } = data;
+		console.log(password, confirmPassword);
+	};
+	return (
+		<form onSubmit={handleSubmit(handleSubmitForm)} className='space-y-4'>
+			<div className='font-bold text-3xl'>Reset Password</div>
+			<div className='text-sm'>
+				Password should meet the following requirements:
+				<ul className='m-0 px-4'>
+					<li>At least 8 characters in length</li>
+					<li>Contains a mix of uppercase and lowercase letters</li>
+					<li>Includes at least one number and one special character</li>
+				</ul>
+			</div>
+			<div>
+				<TextField
+					label='Password*'
+					required={true}
+					name='password'
+					type='password'
+					register={register}
+					requiredMessage={errors.password ? 'Required' : undefined}
+				/>
+			</div>
+			<div>
+				<TextField
+					label='Confirm Password*'
+					required={true}
+					name='confirmPassword'
+					type='password'
+					register={register}
+					requiredMessage={
+						watch('password') === watch('confirmPassword')
+							? undefined
+							: 'Passwords must match'
+					}
+				/>
+			</div>
+			<div>
+				<Button
+					buttonText='Reset Password'
+					buttonTextColor='#000000'
+					buttonColor='#808080'
+				/>
+			</div>
+			<div className='justify-center flex flex-row text-sm'>
+				<Link href='/'> Didn't request to reset password?</Link>
+			</div>
+		</form>
+	);
 };
 
 export default ResetPassword;
