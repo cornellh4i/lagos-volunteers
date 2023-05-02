d266b9e8faf0e170d1293f8936feb574814cb118
bananahz <hrz8@cornell.edu>
2023-05-02 16:48:06 -0400

    add right aligned and styling

--- frontend/src/components/atoms/DropdownSelect.tsx
+++ frontend/src/components/atoms/DropdownSelect.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "./Button";
 import {
   FormControl,
   InputLabel,
@@ -30,51 +31,60 @@ const DropdownSelect = ({
     setValue(event.target.value);
   };
   return (
-    <>
+    <div>
       <FormControl variant="standard" sx={{ m: 1, minWidth: "100%" }}>
         {/* <InputLabel id="demo-simple-select-standard-label">Drafts</InputLabel> */}
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={value}
-          onChange={handleChange}
-          label="Select"
-          autoWidth
-          MenuProps={{
-            MenuListProps: {
-              sx: {
-                padding: 0,
-                borderRadius: 2,
+        <div className="grid grid-cols-1 flex space-y-4">
+          <Button
+            buttonText="Create New Event"
+            buttonTextColor="#000000"
+            buttonColor="#808080"
+          />
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={value}
+            onChange={handleChange}
+            label="Select"
+            autoWidth
+            MenuProps={{
+              MenuListProps: {
+                sx: {
+                  padding: 0,
+                  borderRadius: 2,
+                },
+                // dense: true,
               },
-              // dense: true,
-            },
-            PaperProps: {
-              elevation: 4,
-              sx: {
-                borderRadius: 2,
-                // "& .MuiMenuItem-root": {
-                //   padding: 2,
-                // },
+              PaperProps: {
+                elevation: 4,
+                sx: {
+                  borderRadius: 2,
+                  // "& .MuiMenuItem-root": {
+                  //   padding: 2,
+                  // },
+                },
               },
-            },
-            anchorOrigin: {
-              vertical: "bottom",
-              horizontal: "right",
-            },
-            transformOrigin: {
-              vertical: "top",
-              horizontal: "right",
-            },
-          }}
-        >
-          {/* TODO: set default value to drafts or something */}
-          <MenuItem value="0">{tabs[0]}</MenuItem>
-          <MenuItem value="1">{tabs[1]}</MenuItem>
-          <MenuItem value="2">{tabs[2]}</MenuItem>
-        </Select>
+              anchorOrigin: {
+                vertical: "bottom",
+                horizontal: "right",
+              },
+              transformOrigin: {
+                vertical: "top",
+                horizontal: "right",
+              },
+            }}
+          >
+            <MenuItem value="0">{tabs[0]}</MenuItem>
+            <MenuItem value="1">{tabs[1]}</MenuItem>
+            <MenuItem value="2">{tabs[2]}</MenuItem>
+          </Select>
+        </div>
       </FormControl>
-      <div> {panels[Number(value)]} </div>
-    </>
+      <div className="grid h-screen justify-center">
+        {" "}
+        {panels[Number(value)]}{" "}
+      </div>
+    </div>
   );
 };
 
--- frontend/src/components/molecules/TabContainer.tsx
+++ frontend/src/components/molecules/TabContainer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Tab from "@mui/material/Tab";
 import DropdownSelect from "../atoms/DropdownSelect";
+import Button from "../atoms/Button";
 import { TabPanel, TabContext, TabList } from "@mui/lab";
 
 type TabContainerProps = {
@@ -41,7 +42,7 @@ const TabContainer = ({
 }: TabContainerProps) => {
   // return <>Hello {panels[0]}</>;
   const [width, setWidth] = React.useState(window.innerWidth);
-  const breakpoint = 625;
+  const breakpoint = 800; //cjhange to 420
   React.useEffect(() => {
     /* Inside of a "useEffect" hook add an event listener that updates
        the "width" state variable when the window size changes */
@@ -56,22 +57,40 @@ const TabContainer = ({
     setValue(newValue);
   };
   return width > breakpoint ? (
-    <Box sx={{ width: "100%" }}>
+    <Box sx={{ width: "100%", height: "100%", gap: 2, margin: 2 }}>
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <TabList value={value} onChange={handleChange} aria-label="nav tabs">
-            <Tab label={tabs[0]} value="1" />
-            <Tab label={tabs[1]} value="2" />
-            <Tab label={tabs[2]} value="3" />
-            {/* {tabs.map((tab, i) => (
+          <div className="grid grid-cols-5">
+            <div className="col-start-1 col-end-5">
+              <TabList
+                value={value}
+                onChange={handleChange}
+                aria-label="nav tabs"
+              >
+                <Tab label={tabs[0]} value="1" />
+                <Tab label={tabs[1]} value="2" />
+                <Tab label={tabs[2]} value="3" />
+                {/* {tabs.map((tab, i) => (
               <>
                 <Tab label={tab} value={i.toString()} />
               </>
             ))} */}
-          </TabList>
-          <TabPanel value="1">{panels[0]}</TabPanel>
-          <TabPanel value="2">{panels[1]}</TabPanel>
-          <TabPanel value="3">{panels[2]}</TabPanel>
+              </TabList>
+            </div>
+
+            <div className="col-start-5">
+              <Button
+                buttonText="Create New Event"
+                buttonTextColor="#000000"
+                buttonColor="#808080"
+              />
+            </div>
+          </div>
+          <div className="grid h-screen justify-center">
+            <TabPanel value="1">{panels[0]}</TabPanel>
+            <TabPanel value="2">{panels[1]}</TabPanel>
+            <TabPanel value="3">{panels[2]}</TabPanel>
+          </div>
         </Box>
         {/* {panels.map((panel, i) => (
           <>
