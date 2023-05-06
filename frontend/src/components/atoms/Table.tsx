0138baf9385f944f6fcdfe0f3a5387f84212dd60
JJ <60070699+JJBai2003@users.noreply.github.com>
2023-05-06 03:16:07 -0400

    Frontend for BoxText, Table, and PastEvents (#71)
    * Completed everything
    
    * Format using Prettier
    
    * Refactor
    
    * Refactor chip
    
    * modify table style
    
    * Implement BoxText
    
    ---------
    
    Co-authored-by: JJ Bai <jhb342@cornell.edu>
    Co-authored-by: Jason Zheng <jasonz4200@gmail.com>
    Co-authored-by: Yichen Yao <yy486@cornell.edu>

--- frontend/src/components/atoms/Table.tsx
+++ frontend/src/components/atoms/Table.tsx
@@ -3,7 +3,7 @@ import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
 type TableProps = {
   /** The columns of the table, following the MUI Data Grid spec */
-  columns: GridColDef;
+  columns: GridColDef<object>[];
   /** The table rows represented as an object array */
   rows: Object[];
 };
@@ -12,7 +12,16 @@ type TableProps = {
  * A Table component
  */
 const Table = ({ columns, rows }: TableProps) => {
-  return <>Hello there</>;
+  return (
+    <DataGrid
+      columns={columns}
+      rows={rows}
+      sx={{ border: 0 }}
+
+      // getRowId={(r) => r.DT_RowId}
+      // onCellClick={{console.log(getRowId)}}
+    />
+  );
 };
 
 export default Table;
--- frontend/src/components/molecules/ViewEvents.tsx
+++ frontend/src/components/molecules/ViewEvents.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import TabContainer from "./TabContainer";
+import BoxText from "@/components/atoms/BoxText";
+import Chip from "@/components/atoms/Chip";
+import TabContainer from "@/components/molecules/TabContainer";
+import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import Table from "@/components/atoms/Table";
 import Button from "../atoms/Button";
 
 const UpcomingEvents = () => {
@@ -9,8 +13,67 @@ const UpcomingEvents = () => {
 const Drafts = () => {
   return <>Hello drafts</>;
 };
+
 const PastEvents = () => {
-  return <>Hello past</>;
+  const eventColumns: GridColDef[] = [
+    {
+      field: "role",
+      headerName: "Role",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <Chip
+            color={params.value == "Supervisor" ? "primary" : "success"}
+            label={params.value}
+          />
+        );
+      },
+    },
+    {
+      field: "program",
+      headerName: "Program Name",
+      width: 800,
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      type: "date",
+      width: 110,
+    },
+    {
+      field: "hours",
+      headerName: "Hours",
+      type: "number",
+      width: 110,
+    },
+  ];
+
+  // below are dummy data, in the future we want to get data from backend and
+  // format them like this
+  let dummyDate: Date = new Date(2023, 0o1, 21);
+  const dummyRows = [
+    {
+      id: 1,
+      role: "Supervisor",
+      program: "EDUFOOD",
+      date: dummyDate,
+      hours: 4,
+    },
+    {
+      id: 2,
+      role: "Volunteer",
+      program: "Malta Outreach",
+      date: dummyDate,
+      hours: 4,
+    },
+  ];
+
+  return (
+    <div>
+      <BoxText text="Volunteer Hours" textRight="30" />
+      <Table columns={eventColumns} rows={dummyRows} />
+    </div>
+  );
 };
 
 /**
