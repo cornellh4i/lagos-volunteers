529fca12ae94a2dc86badc472167449250b66db5
Jason Zheng <jasonz4200@gmail.com>
2023-05-16 23:10:26 -0400

    Add table styling

--- frontend/src/components/atoms/Table.tsx
+++ frontend/src/components/atoms/Table.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridColDef,
+  GridColumnHeaderParams,
+  GridValueGetterParams,
+  MuiEvent,
+} from "@mui/x-data-grid";
 
 type TableProps = {
   /** The columns of the table, following the MUI Data Grid spec */
@@ -17,6 +23,16 @@ const Table = ({ columns, rows }: TableProps) => {
       columns={columns}
       rows={rows}
       sx={{ border: 0 }}
+      disableRowSelectionOnClick
+      initialState={{
+        pagination: {
+          paginationModel: {
+            pageSize: 100,
+          },
+        },
+      }}
+      pageSizeOptions={[]}
+      disableColumnMenu
 
       // getRowId={(r) => r.DT_RowId}
       // onCellClick={{console.log(getRowId)}}
