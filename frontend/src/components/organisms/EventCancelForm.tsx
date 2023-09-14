7e9f6fad697658715ff4b417f2263d6de32f7e22
Jason Zheng <jasonz4200@gmail.com>
2023-09-14 07:06:38 -0400

    Add skeleton for EventCancelForm

---
+++ frontend/src/components/organisms/EventCancelForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface EventCancelFormProps {
+  eventid: string;
+}
+
+/**
+ * An EventCancelForm component
+ */
+const EventCancelForm = ({ eventid }: EventCancelFormProps) => {
+  return <>Hello there</>;
+};
+
+export default EventCancelForm;
