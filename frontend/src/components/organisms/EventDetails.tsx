d3c572139200148739158e45d937137465fd8113
Yichen Yao <yy486@cornell.edu>
2023-09-17 00:35:25 +0000

    implement event details, modify icon for color, implement responsiveness

--- frontend/src/components/organisms/EventDetails.tsx
+++ frontend/src/components/organisms/EventDetails.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import IconText from "../atoms/IconText"
+import Chip from "../atoms/Chip"
+
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import PersonIcon from '@mui/icons-material/Person';
 
 interface EventDetailsProps {
   title: string;
@@ -7,6 +13,7 @@ interface EventDetailsProps {
   supervisors: string[];
   capacity: number;
   image: React.ReactElement;
+  tags: string[];
 }
 
 /**
@@ -20,8 +27,35 @@ const EventDetails = ({
   supervisors,
   capacity,
   image,
+  tags
 }: EventDetailsProps) => {
-  return <>Hello there</>;
+
+  
+  var supervisor= supervisors.toString().replace(",",", ");
+  return (
+    <div>
+      <div className="text-2xl font-semibold mb-6">{title}</div>
+      <div className="sm:flex md:space-x-8">
+        <div className="flex-left">
+          {image}
+        </div>
+        <div className="flex-left">
+          <div className="text-lg font-semibold mb-4 mt-4 md:mt-0">Event Details</div>
+          <div className="space-y-0.5 mb-4">
+            <IconText icon={<LocationOnIcon/>} 
+             text={"LOCATION ("+location+")"} color="text-gray-400"/>
+            <IconText icon={<CalendarMonthIcon/>} 
+             text={datetime} color="text-gray-400"/>
+            <IconText icon={<PersonIcon/>} 
+             text={"Supervisor: "+supervisor} color="text-gray-400"/>
+            <IconText icon={<PersonIcon/>} 
+             text={"Event Capacity: "+capacity.toString()} color="text-gray-400"/>
+          </div>
+          <div className="flex space-x-2"> {tags.map(tag=>{return <Chip label={tag} color="default" />})}</div>
+        </div>
+      </div> 
+    </div>
+  );
 };
 
 export default EventDetails;
