71cef9bc3fc02f74991874aebeed7767d53a5e58
Yichen Yao <yy486@cornell.edu>
2023-09-18 20:00:30 -0400

    revert icontext

--- frontend/src/components/atoms/IconText.tsx
+++ frontend/src/components/atoms/IconText.tsx
@@ -2,18 +2,17 @@ import React from "react";
 
 interface IconTextProps {
   icon: React.ReactElement;
-  color: string;
   text: string;
 }
 
 /**
  * A IconText component is a small line of text prefaced by an icon
  */
-const IconText = ({ icon, color, text }: IconTextProps) => {
+const IconText = ({ icon, text }: IconTextProps) => {
   return (
     <>
       <div className="flex items-center">
-        <div className={"flex-left "+color}>{icon}</div>
+        <div className={"flex-left"}>{icon}</div>
         <div className="flex-left">{text}</div>
       </div>
     </>
