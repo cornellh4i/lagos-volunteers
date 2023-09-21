13c162854667d0398fc4af575b6981c692b20a1f
Louis Valencia <lcv28@cornell.edu>
2023-09-21 14:52:14 +0000

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -9,7 +9,12 @@ const About = () => {
         <p className="tracking-tighter">Hello</p>
       </div>
       <Button text="hello" />
-      Hello there
+
+      <div className="grid grid-cols-1 sm:grid-cols-2">
+        <div className="bg-red-600">Hello</div>
+        <div className="bg-blue-600">Hello</div>
+        <div className="bg-green-600">Hello</div>
+      </div>
     </>
   );
 };
