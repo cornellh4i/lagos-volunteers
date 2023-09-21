c7ea0a078e812649290073db59779942c2991c16
Louis Valencia <lcv28@cornell.edu>
2023-09-21 13:38:08 +0000

    Complete props onboarding

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-/** A simple Button component */
-const Button = () => {
-  return (
-    <button>Press me</button>
-  );
+/**
+ * Component for a general button
+ * @param text is the text on the button
+ */
+
+interface Props {
+  text: string;
+}
+
+const Button = ({ text }: Props) => {
+  return <button>{text}</button>;
 };
 
 export default Button;
