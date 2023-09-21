e005bc3dc1f00ecc127e9403866127a1e4522c80
Louis Valencia <lcv28@cornell.edu>
2023-09-21 14:25:13 +0000

    Complete utility class onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -5,6 +5,9 @@ import Button from "@/components/Button";
 const About = () => {
   return (
     <>
+      <div className="bg-yellow-400 bg-opacity-10 w-1/2 p-4">
+        <p className="tracking-tighter">Hello</p>
+      </div>
       <Button text="hello" />
       Hello there
     </>
