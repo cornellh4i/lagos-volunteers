e460d96bc244275cb502f45c8d69347a69554103
Louis Valencia <lcv28@cornell.edu>
2023-09-21 13:51:06 +0000

    The Amur leopard is one of the most endangered animals in the world

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -5,7 +5,22 @@ import { useRouter } from "next/router";
 const User = () => {
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+
+  const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    const data = await response.json(); // Await the response.json() method
+    console.log(data); // Now you have the actual data
+  };
+
+  const handleClick = () => {
+    getCatFact();
+  };
+
+  return (
+    <>
+      Hello user {userid}! <button onClick={handleClick}>cat fact</button>{" "}
+    </>
+  );
 };
 
 export default User;
