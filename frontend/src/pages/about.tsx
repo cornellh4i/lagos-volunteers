1beeeccdad3ad0e678f0d717e37ec635850efe4d
Daniel Baldeo-Thorne <dsb289@cornell.edu>
2023-09-23 02:56:57 +0000

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -8,6 +8,13 @@ const About = () => {
       <div className="bg-yellow-200 bg-opacity-1 w-1/2">
         <p className="tracking-tighter">Hello</p>
       </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 ">
+        <div className="bg-red-600 rounded-lg shadow-xl ">Hello</div>
+        <div className="bg-blue-600 rounded-lg shadow-xl ">Hello</div>
+        <div className="bg-green-600 rounded-lg shadow-xl sm:col-span-3">
+          Hello
+        </div>
+      </div>
       <Button text="hello" />
       Hello there ;
     </>
