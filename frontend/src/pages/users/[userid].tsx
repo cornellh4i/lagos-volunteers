ffc36ec52cc130eafdd49799496a7eeae4c7cd04
Daniel Baldeo-Thorne <dsb289@cornell.edu>
2023-09-23 00:55:39 +0000

    The cat appears to be the only domestic companion animal not mentioned in the Bible.

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -5,7 +5,21 @@ import { useRouter } from "next/router";
 const User = () => {
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+
+  const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    const data = await response.json();
+    console.log(data);
+  };
+
+  const handleClick = () => {
+    getCatFact();
+  };
+  return (
+    <>
+      Hello user {userid}!<button onClick={handleClick}>cat fact</button>{" "}
+    </>
+  );
 };
 
 export default User;
