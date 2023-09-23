062b28a658b14608c9c8e3509264d1520b4107e7
Daniel Baldeo-Thorne <dsb289@cornell.edu>
2023-09-23 01:09:32 +0000

    Complete utility class onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -5,8 +5,11 @@ import Button from "@/components/Button";
 const About = () => {
   return (
     <>
+      <div className="bg-yellow-200 bg-opacity-1 w-1/2">
+        <p className="tracking-tighter">Hello</p>
+      </div>
       <Button text="hello" />
-      Hello there
+      Hello there ;
     </>
   );
 };
