ba620abd2294daae7bdeb6e16186b33ab0f6f634
Sophie <slw284@cornell.edu>
2023-09-23 23:46:38 -0400

    Co-authored-by: snehar27 <snehar27@users.noreply.github.com>

--- frontend/src/components/molecules/Modal.tsx
+++ frontend/src/components/molecules/Modal.tsx
@@ -1,38 +1,72 @@
 import React from "react";
-import { Modal }  from "@mui/material"; 
+import { Modal, Grid, Backdrop, Fade, Box, Typography }  from "@mui/material"; 
 import Button from "../../components/atoms/Button";
+import style from "styled-jsx/style";
 
 interface CustomModalProps {
   open: boolean;
-  header: string;
-  bodyText: string;
-  buttonText: string;
-  buttonText2: string;
+  body: React.ReactElement;
 }
-
-
 
 /**
  * A Modal component is a styled modal that takes in a body component
  */
-const CustomModal = ({ header, bodyText, buttonText, buttonText2 }: CustomModalProps) => {
-  const [open, setOpen] = React.useState(false);
+const CustomModal = ({ body }: CustomModalProps) => {
+  const [open, setOpen] = React.useState(true);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  return (
-    <div>
-    <Modal
-    open={open}
-    onClose={handleClose}>
-  <div>
-        <h2 id="child-modal-title"> header </h2>
-        <p id="child-modal-description">
-          bodyText
+      return (
+        <>
+        <Button onClick={handleOpen} color="gray">Open modal</Button>
+        <div className="flex justify-center items-center h-screen">
+
+        <Grid container alignItems="center">
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            open={open}
+            onClose={handleClose}
+          >
+            <Box sx={{
+              width: 300,
+              height: 300,
+              backgroundColor: 'gray',
+              '&:hover': {
+                backgroundColor: 'primary.main',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+              justifyContent="center">
+              <Typography id="transition-modal-title" variant="h6" component="h2">
+                Text in a modal
+              </Typography>
+              <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
+          </Grid>
+        </div></>
+    // <div className="p-52">
+    // <Modal
+    // open={open}
+    // onClose={handleClose}>
+  /* <div>
+        <h2 id="modal-title"> {header} </h2>
+        <p id="modal-description">
+          {bodyText}
         </p>
-        <Button color="gray" onClick={handleClose}>buttonText</Button>
-      </div>
+        <Grid item container>
+          <Grid xs={6}>
+        <Button color="gray" onClick={handleClose}>{buttonText}</Button>
+        </Grid>
+        <Grid xs={6}>
+        <Button color="gray" onClick={handleClose}>{buttonText}</Button>
+        </Grid>
+        </Grid>
+        </div>
   </Modal>
-  </div>
+  </div> */
   );
 };
 
