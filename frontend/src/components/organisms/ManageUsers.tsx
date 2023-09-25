b7177a8d7cfc30869de3c020b271be4a1e602f45
Jason Zheng <jasonz4200@gmail.com>
2023-09-25 02:02:45 -0400

    Prepare for second sprint

---
+++ frontend/src/components/organisms/ManageUsers.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface ManageUsersProps {}
+
+/**
+ * A ManageUsers component
+ */
+const ManageUsers = ({}: ManageUsersProps) => {
+  return (
+    <>
+      <div>Lorem</div>
+    </>
+  );
+};
+
+export default ManageUsers;
