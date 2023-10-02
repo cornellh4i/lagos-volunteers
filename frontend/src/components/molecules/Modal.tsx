58fdf5805bbeb7b6dd5dfcb36d1f7918ca8ceb0d
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2023-10-02 12:04:25 -0400

    little styling

--- frontend/src/components/molecules/Modal.tsx
+++ frontend/src/components/molecules/Modal.tsx
@@ -1,57 +1,67 @@
-import React from 'react';
-import { Modal, Grid, Backdrop, Fade, Box, Typography } from '@mui/material';
-import Button from '../../components/atoms/Button';
-import { StringLiteral } from 'typescript';
+import React from "react";
+import { Modal, Grid, Backdrop, Fade, Box, Typography } from "@mui/material";
+import Button from "../../components/atoms/Button";
+import { StringLiteral } from "typescript";
 
 interface CustomModalProps {
 	open: boolean;
-	title: string; 
-	bodyText: string; 
-	leftButtonText: string; 
+	title: string;
+	bodyText: string;
+	leftButtonText: string;
 	rightButtonText: string;
 }
 
 const style = {
-	position: 'absolute' as 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	bgcolor: 'background.paper',
-	borderRadius: '5px',
-	width : '70%',
-	p: 4,
-	fontweight: 'bold'
+	position: "absolute" as "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	bgcolor: "background.paper",
+	borderRadius: "5px",
+	width: "50%",
+	paddingLeft: 10,
+	paddingRight: 10,
+	paddingTop: 5,
+	paddingBottom: 5,
+	fontweight: "bold",
 };
 
 /**
  * A Modal component is a styled modal that takes in a body component
  */
-const CustomModal = ({ title, bodyText, leftButtonText, rightButtonText }: CustomModalProps) => {
+const CustomModal = ({
+	title,
+	bodyText,
+	leftButtonText,
+	rightButtonText,
+}: CustomModalProps) => {
 	const [open, setOpen] = React.useState(true);
 	const handleOpen = () => setOpen(true);
 	const handleClose = () => setOpen(false);
 	return (
-			<Modal
-				className='w-full'
-				open={open}
-				onClose={handleClose}
-				aria-labelledby='modal-modal-title'
-				aria-describedby='modal-modal-description'>
-				<Box sx={style}>
-					<b>{title}</b>
-					<br></br>
-					{bodyText}
-					<br></br>
-					<Grid container spacing={2}>
+		<Modal
+			className="w-full"
+			open={open}
+			onClose={handleClose}
+			aria-labelledby="modal-modal-title"
+			aria-describedby="modal-modal-description">
+			<Box sx={style}>
+				<h2>{title}</h2>
+				<p>{bodyText}</p>
+				<Grid container spacing={2}>
 					<Grid item md={6} xs={12}>
-					<Button color= "gray" type="button" onClick={handleClose}>{leftButtonText}</Button>
+						<Button color="gray" type="button" onClick={handleClose}>
+							{leftButtonText}
+						</Button>
 					</Grid>
 					<Grid item md={6} xs={12}>
-					<Button color='dark-gray' type='button' onClick={handleClose}>{rightButtonText}</Button>
+						<Button color="dark-gray" type="button" onClick={handleClose}>
+							{rightButtonText}
+						</Button>
 					</Grid>
-					</Grid>
-				</Box>
-			</Modal>
+				</Grid>
+			</Box>
+		</Modal>
 	);
 };
 
--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,16 +1,22 @@
-import React from "react"; 
+import React from "react";
 import CustomModal from "../components/molecules/Modal";
 import Button from "../components/atoms/Button";
-import { Box } from '@mui/material';
-
+import { Box } from "@mui/material";
 
 const test = () => {
-    return (
-        <div>
-        <CustomModal open = {true} title={"Title"} bodyText={"Lorem ipsum du bio bo"} leftButtonText={"Disagree"} rightButtonText={"Agree & Continue"}
-        />
-        </div>
-    );
+	return (
+		<div>
+			<CustomModal
+				open={true}
+				title={"Terms and Conditions"}
+				bodyText={
+					"By registering, I agree that I will attend the event. If I cannot attend, I will cancel my registration at least 24 hours before the event begins. Failure to cancel my registration may negatively impact my status as a volunteer"
+				}
+				leftButtonText={"Disagree"}
+				rightButtonText={"Agree & Continue"}
+			/>
+		</div>
+	);
 };
 
 export default test;
