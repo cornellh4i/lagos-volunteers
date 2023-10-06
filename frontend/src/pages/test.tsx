e5313b59096b3c7e7adaee6bcd9936d04e9f09d6
DannyThorne01 <90882246+DannyThorne01@users.noreply.github.com>
2023-10-06 17:48:34 -0400

    Delete frontend/src/pages/test.tsx

--- frontend/src/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import EventCancelForm from "@/components/organisms/ManageAttendees";
-import ManageAttendees from "@/components/organisms/ManageAttendees";
-
-const Test = () => {
-  return <ManageAttendees />;
-};
-
-export default Test;
