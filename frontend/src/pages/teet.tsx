fe76762b544249d0c5cd3ad3b42dbb0bb81307a5
DannyThorne01 <90882246+DannyThorne01@users.noreply.github.com>
2023-10-06 16:30:50 -0400

    Delete frontend/src/pages/teet.tsx

--- frontend/src/pages/teet.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import EventCancelForm from "@/components/organisms/EventCancelForm";
-
-const Teet = () => {
-  return <EventCancelForm eventid="1" />;
-};
-
-export default Teet;
