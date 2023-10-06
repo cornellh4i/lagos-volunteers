95c752de27d10263288b3971bd496018f061b4c7
Sophie <slw284@cornell.edu>
2023-10-06 21:15:24 +0000

    added responsive design

--- frontend/src/components/molecules/Modal.tsx
+++ frontend/src/components/molecules/Modal.tsx
@@ -4,65 +4,81 @@ import Button from "../../components/atoms/Button";
 import { StringLiteral } from "typescript";
 
 interface CustomModalProps {
-	open: boolean;
-	title: string;
-	bodyText: string;
-	leftButtonText: string;
-	rightButtonText: string;
+  open: boolean;
+  handleClose: () => void;
+  children: React.ReactElement;
 }
 
 const style = {
-	position: "absolute" as "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	bgcolor: "background.paper",
-	borderRadius: "5px",
-	width: "50%",
-	paddingLeft: 10,
-	paddingRight: 10,
-	paddingTop: 5,
-	paddingBottom: 5,
-	fontweight: "bold",
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  borderRadius: "5px",
+  width: "50%",
+  paddingLeft: 10,
+  paddingRight: 10,
+  paddingTop: 5,
+  paddingBottom: 5,
+  fontweight: "bold",
+};
+
+const styleSmall = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  borderRadius: "5px",
+  width: "80%",
+  paddingLeft: 5,
+  paddingRight: 5,
+  paddingTop: 5,
+  paddingBottom: 5,
+  fontweight: "bold",
 };
 
 /**
  * A Modal component is a styled modal that takes in a body component
  */
-const CustomModal = ({
-	title,
-	bodyText,
-	leftButtonText,
-	rightButtonText,
-}: CustomModalProps) => {
-	const [open, setOpen] = React.useState(true);
-	const handleOpen = () => setOpen(true);
-	const handleClose = () => setOpen(false);
-	return (
-		<Modal
-			className="w-full"
-			open={open}
-			onClose={handleClose}
-			aria-labelledby="modal-modal-title"
-			aria-describedby="modal-modal-description">
-			<Box sx={style}>
-				<h2>{title}</h2>
-				<p>{bodyText}</p>
-				<Grid container spacing={2}>
-					<Grid item md={6} xs={12}>
-						<Button color="gray" type="button" onClick={handleClose}>
-							{leftButtonText}
-						</Button>
-					</Grid>
-					<Grid item md={6} xs={12}>
-						<Button color="dark-gray" type="button" onClick={handleClose}>
-							{rightButtonText}
-						</Button>
-					</Grid>
-				</Grid>
-			</Box>
-		</Modal>
-	);
-}; 
+const CustomModal = ({ open, handleClose, children }: CustomModalProps) => {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 420;
+  React.useEffect(() => {
+    /* Inside of a "useEffect" hook add an event listener that updates
+       the "width" state variable when the window size changes */
+    window.addEventListener("resize", () => setWidth(window.innerWidth));
+
+    /* passing an empty array as the dependencies of the effect will cause this
+       effect to only run when the component mounts, and not each time it updates.
+       We only want the listener to be added once */
+  }, []);
+  const [value, setValue] = React.useState("1");
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+  return width > breakpoint ? (
+    <Modal
+      className="w-full"
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>{children}</Box>
+    </Modal>
+  ) : (
+    <Modal
+      className="w-full"
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={styleSmall}>{children}</Box>
+    </Modal>
+  );
+};
 
 export default CustomModal;
--- frontend/src/pages/test.tsx
+++ frontend/src/pages/test.tsx
@@ -1,22 +1,52 @@
 import React from "react";
 import CustomModal from "../components/molecules/Modal";
 import Button from "../components/atoms/Button";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
+import MuiButton from "@mui/material/Button";
+
+const ModalBody = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div>
+      <h2>Terms and Conditions</h2>
+      <p>
+        By registering, I agree that I will attend the event. If I cannot
+        attend, I will cancel my registration at least 24 hours before the event
+        begins. Failure to cancel my registration may negatively impact my
+        status as a volunteer
+      </p>
+      <Grid container spacing={2}>
+        <Grid item md={6} xs={12}>
+          <Button color="gray" type="button" onClick={handleClose}>
+            Disagree
+          </Button>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Button color="dark-gray" type="button" onClick={handleClose}>
+            Agree & Continue
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 const test = () => {
-	return (
-		<div>
-			<CustomModal
-				open={true}
-				title={"Terms and Conditions"}
-				bodyText={
-					"By registering, I agree that I will attend the event. If I cannot attend, I will cancel my registration at least 24 hours before the event begins. Failure to cancel my registration may negatively impact my status as a volunteer"
-				}
-				leftButtonText={"Disagree"}
-				rightButtonText={"Agree & Continue"}
-			/>
-		</div>
-	);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  return (
+    <div>
+      <MuiButton onClick={handleOpen}>Open modal</MuiButton>
+      <CustomModal
+        open={open}
+        handleClose={handleClose}
+        children={<ModalBody />}
+      />
+    </div>
+  );
 };
 
 export default test;
