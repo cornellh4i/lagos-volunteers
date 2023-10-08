d8403a2c010243063d7bd8dbcd009744c5c90930
Akinfolami Akin-Alamu <59776300+akinfelami@users.noreply.github.com>
2023-10-08 16:51:00 -0400

    User registration & Authentication Flow (#100)
    * move login to page level auth
    
    * Pages no longer flashing
    
    * move login and signup check to welcome template
    
    * login functionality now working
    
    * error codes now  show if incorrect credentials
    
    * Add authenticating page
    
    * experiment with full-stack registration
    
    * user reg works now
    
    * minor all-round changes for registration validation
    
    * user reg not there yet
    
    * custom create user with email working
    
    * Can we move Continue with google to MVP?
    
    * reset password working
    
    * update password & profile logic
    
    * changes to profile
    
    * syntax changes + jest ci upgrade
    
    * upgrade jest
    
    * remove node 17 from jest ci
    
    * prisma accept data loss (posgtres container is destroyes anyway)
    
    * docker stuff
    
    * Reformat tab indents to spaces
    
    * Rename CustomAlert
    
    * Refactor Loading
    
    * Revert accidental change
    
    * Decouple auth from WelcomeTemplate
    
    * More decoupling
    
    * Redirect index to events/view
    
    * Move docker files into docker folder
    
    * Disable broken event cards
    
    * minor change to dockerfile
    
    * Simplify error components
    
    * fix
    
    * Restore original styles
    
    * minor tweak
    
    * fix
    
    * Fix alerts
    
    * Rename
    
    ---------
    
    Co-authored-by: Jason Zheng <jasonz4200@gmail.com>

---
+++ frontend/src/components/molecules/Loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+
+function Loading() {
+  return (
+    <div className="h-screen bg-white">
+      <div className="flex justify-center items-center h-full">
+        <CircularProgress />
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
