81098824641e8567cba28de6452e929d4eb4f861
Louis Valencia <lcv28@cornell.edu>
2023-10-09 00:30:16 +0000

    Bolded headers and fixed spacing

--- frontend/src/components/organisms/ManageUsers.tsx
+++ frontend/src/components/organisms/ManageUsers.tsx
@@ -5,7 +5,7 @@ import Button from "../atoms/Button";
 import { GridColDef } from "@mui/x-data-grid";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import SearchBar from "@/components/atoms/SearchBar";
-
+// ADD MINWIDTH
 interface ManageUsersProps {}
 /**
  * A ManageUsers component
@@ -17,27 +17,47 @@ const Active = () => {
       field: "name",
       headerName: "Name",
       flex: 1,
+      minWidth: 100,
+      renderHeader: (params) => (
+        <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
+      ),
     },
     {
       field: "email",
       headerName: "Email",
       flex: 1,
+      minWidth: 100,
+      renderHeader: (params) => (
+        <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
+      ),
     },
     {
       field: "role",
       headerName: "Role",
       flex: 1,
+      minWidth: 100,
+      renderHeader: (params) => (
+        <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
+      ),
     },
     {
       field: "date",
       headerName: "Joined on",
       flex: 1,
+      minWidth: 100,
       type: "date",
+      renderHeader: (params) => (
+        <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
+      ),
     },
     {
       field: "hours",
       headerName: "Total hours",
       flex: 2,
+      minWidth: 100,
+      renderHeader: (params) => (
+        <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
+      ),
     },
     {
       headerName: "",
@@ -52,10 +72,7 @@ const Active = () => {
             justifyContent: "flex-end",
           }}
         >
-          <Button color="gray">
-            {/* <AccountBoxIcon />   icon for View Profile*/}
-            View Profile
-          </Button>
+          <Button color="gray">View Profile</Button>
         </div>
       ),
     },
@@ -80,11 +97,123 @@ const Active = () => {
       date: dummyDate,
       hours: 20 + " hours",
     },
+    {
+      id: 3,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 4,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 5,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 6,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 7,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 8,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 9,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 10,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 11,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 12,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 13,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 14,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 15,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
+    {
+      id: 16,
+      name: "Julia Papp",
+      email: "jpapp@gmail.com",
+      role: "Volunteer",
+      date: dummyDate,
+      hours: 20 + " hours",
+    },
   ];
 
   return (
     <div>
-      <Table columns={eventColumns} rows={dummyRows} />
+      <Table columns={eventColumns} rows={dummyRows} boldHeader={true} />
     </div>
   );
 };
@@ -98,7 +227,9 @@ const ManageUsers = ({}: ManageUsersProps) => {
     <>
       <div className="font-semibold text-3xl">Manage Members</div>
       <br></br> {/* add search bar */}
-      <TabContainer tabs={tabs} />
+      <div>
+        <TabContainer tabs={tabs} />
+      </div>
     </>
   );
 };
