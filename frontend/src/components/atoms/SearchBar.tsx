a3ee9752a296e3ac2abe0e928a45cf5c4a145d28
Jason Zheng <jasonz4200@gmail.com>
2023-10-21 11:51:24 -0400

    Separate props out and refactor code

--- frontend/src/components/atoms/SearchBar.tsx
+++ frontend/src/components/atoms/SearchBar.tsx
@@ -1,32 +1,46 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent } from "react";
 // import { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-import { Paper, InputBase } from "@mui/material";
+import { Paper, InputBase, InputAdornment } from "@mui/material";
 
 interface SearchBarProps {
+  placeholder: string;
   value: string;
+  onChange: (event: ChangeEvent<any>) => void;
+  onClick: (event: FormEvent<any>) => void;
 }
 
 /**
  * A SearchBar component
  */
-const SearchBar = ({ value }: SearchBarProps) => {
+const SearchBar = ({
+  value,
+  placeholder,
+  onChange,
+  onClick,
+}: SearchBarProps) => {
   return (
     <>
       <Paper
         component="form"
+        onClick={onClick}
         variant="outlined"
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+        sx={{ display: "flex", alignItems: "center" }}
       >
         <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Search member by name, email"
-          inputProps={{ 'aria-label': 'search google maps' }}
+          sx={{ ml: 1, p: 0.5, flex: 1 }}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton type="submit" aria-label="search">
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          }
         />
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-          <SearchIcon />
-        </IconButton>
       </Paper>
     </>
   );
