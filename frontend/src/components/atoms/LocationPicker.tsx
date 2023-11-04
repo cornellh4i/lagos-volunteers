1b8b0a7ade45f6d7e2f7aad4f568913f6469682f
bananahz <hrz8@cornell.edu>
2023-11-04 15:27:26 -0400

    boiler plate code

--- frontend/src/components/atoms/LocationPicker.tsx
+++ frontend/src/components/atoms/LocationPicker.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import { TextField } from "@mui/material";
+import { useRef, useState } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
+
 
 interface LocationPickerProps {
   label: string;
@@ -11,21 +14,42 @@ interface LocationPickerProps {
  * geographic location with autocomplete
  */
 const LocationPicker = ({ label }: LocationPickerProps) => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const inputStyle = {
+    boxShadow: "inset 0 0 10px #eee !important",
+    border: "2px solid #eee",
+    width: "456px",
+    height: "40px",
+    marginLeft: "16px",
+    borderRadius: "20px",
+    fontWeight: "300 !important",
+    outline: "none",
+    padding: "10px 20px",
+    marginBottom: "10px",
+  };
+
+  const autoComplete = new google.maps.places.Autocomplete(
+    inputRef.current!
+  );
+
+  autoComplete.addListener("place_changed", () => {
+    const place = autoComplete.getPlace();
+    if (!place.geometry || !place.geometry.location) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      alert("this location not available");
+    }
+    if (place.geometry && (place.geometry.viewport || place.geometry.location)) {
+      // do something
+      console.log(place.geometry.location);
+    }
+  });
+
   return (
     <div>
-      <div> {label} </div>
-      <Autocomplete
-        disablePortal
-        options={LocationOptions}
-        sx={{
-          borderRadius: 2,
-          borderColor: "primary.main",
-          size: "small",
-          margin: "dense",
-        }}
-        size="small"
-        renderInput={(params) => <TextField {...params} />}
-      />
+      <input placeholder="type your location" ref={inputRef} style={inputStyle} />
+      <div id="map" />
+      <script src="https://maps.googleapis.com/maps/api/js?key=yourMapsAPIKey&libraries=geometry"></script>
     </div>
   );
 };
