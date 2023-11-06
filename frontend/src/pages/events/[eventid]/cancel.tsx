3be277be5527df1adf4e6e7b4fad5ed0bbaea0e8
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2023-11-06 01:53:22 -0500

    [WIP]: remove changes from events folder

--- frontend/src/pages/events/[eventid]/cancel.tsx
+++ frontend/src/pages/events/[eventid]/cancel.tsx
@@ -1,134 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import CenteredTemplate from "@/components/templates/CenteredTemplate";
 import EventCancelForm from "@/components/organisms/EventCancelForm";
-import EventConfirmation from "@/components/organisms/EventConfirmation";
-import { BASE_URL } from "@/utils/constants";
-import { auth } from "@/utils/firebase";
-import { useAuth } from "@/utils/AuthContext";
-import { fetchUserIdFromDatabase } from "@/utils/helpers";
-
-type eventData = {
-	eventid: string;
-	location: string;
-	datetime: string;
-	supervisors: string[];
-	capacity: number;
-	image_src: string;
-	tags: string[] | undefined;
-};
-
-function formatDateTimeRange(startDateString: string, endDateString: string) {
-	const startDate = new Date(startDateString);
-	const endDate = new Date(endDateString);
-
-	const startDateFormatted = `${
-		startDate.getUTCMonth() + 1
-	}/${startDate.getUTCDate()}/${startDate.getUTCFullYear()}`;
-	const startTimeFormatted = formatUTCTime(startDate);
-	const endTimeFormatted = formatUTCTime(endDate);
-
-	const formattedDateTimeRange = `${startDateFormatted}, ${startTimeFormatted} - ${endTimeFormatted}`;
-
-	return formattedDateTimeRange;
-}
-
-function formatUTCTime(date: Date) {
-	const hours = date.getUTCHours();
-	const minutes = date.getUTCMinutes();
-
-	const period = hours < 12 ? "AM" : "PM";
-	const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-	const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-
-	return `${formattedHours}:${formattedMinutes} ${period}`;
-}
 
 /** An EventCancellation page */
 const EventCancellation = () => {
-	const router = useRouter();
-	const { eventid } = router.query;
-	const [eventDetails, setEventDetails] = useState<
-		eventData | null | undefined
-	>(null);
-	const [attendeeCanceled, setAttendeeCanceled] = useState<boolean>(false);
+  const router = useRouter();
+  const { eventid } = router.query;
 
-	const { user } = useAuth();
-	const url = BASE_URL as string;
-
-	const fetchEventDetails = async () => {
-		const userToken = await auth.currentUser?.getIdToken();
-
-		try {
-			const url = BASE_URL as string;
-			const userId = await fetchUserIdFromDatabase(
-				user?.email as string,
-				userToken as string
-			);
-			const fetchUrl = `${url}/users/${userId}/registered?eventid=${eventid}`;
-
-			const response = await fetch(fetchUrl, {
-				method: "GET",
-				headers: {
-					Authorization: `Bearer ${userToken}`,
-				},
-			});
-
-			const data = await response.json();
-
-			const event = data["data"]["eventDetails"];
-
-			if (response.ok) {
-				setEventDetails({
-					eventid: event["id"],
-					location: event["location"],
-					datetime: formatDateTimeRange(event["startDate"], event["endDate"]),
-					supervisors: [
-						event["owner"]["profile"]["firstName"] +
-							" " +
-							event["owner"]["profile"]["lastName"],
-					],
-					capacity: event["capacity"],
-					image_src: event["imageURL"],
-					tags: event["tags"],
-				});
-
-				if (data["data"]["attendance"]) {
-					setAttendeeCanceled(data["data"]["attendance"]["canceled"]);
-				}
-
-				// If attendees is empty -> route to register form
-				if (attendeeCanceled || !data["data"]["attendance"]) {
-					router.replace(`/events/${eventid}/register`);
-				}
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	useEffect(() => {
-		fetchEventDetails();
-	}, []);
-
-	return (
-		<CenteredTemplate>
-			{eventDetails ? (
-				// if attendees has 1 entry and if canceled is True -> return EventCofirmation
-				attendeeCanceled ? (
-					<EventConfirmation
-						eventDetails={eventDetails}
-						confirmation="cancel"
-					/>
-				) : (
-					// if attendees has 1 entry and if canceled is False -> return EventCancelForm
-					<EventCancelForm eventDetails={eventDetails} />
-				)
-			) : (
-				<div>Getting your data...</div>
-			)}
-		</CenteredTemplate>
-	);
+  return (
+    <CenteredTemplate>
+      <EventCancelForm
+        eventid={eventid as string}
+        location="Address, Building Name"
+        datetime="02/15/2023, 9:00AM-11:00AM"
+        supervisors={["Jane Doe", "Jess Lee"]}
+        capacity={20}
+        image_src="https://i0.wp.com/roadmap-tech.com/wp-content/uploads/2019/04/placeholder-image.jpg?resize=800%2C800&ssl=1"
+        tags={["In-person", "EDUFOOD"]}
+      />
+    </CenteredTemplate>
+  );
 };
 
 export default EventCancellation;
--- frontend/src/pages/events/[eventid]/register.tsx
+++ frontend/src/pages/events/[eventid]/register.tsx
@@ -1,128 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import CenteredTemplate from "@/components/templates/CenteredTemplate";
 import EventRegisterForm from "@/components/organisms/EventRegisterForm";
-import EventConfirmation from "@/components/organisms/EventConfirmation";
-import { BASE_URL } from "@/utils/constants";
-import { auth } from "@/utils/firebase";
-import { useAuth } from "@/utils/AuthContext";
-import { fetchUserIdFromDatabase } from "@/utils/helpers";
-
-type eventData = {
-	eventid: string;
-	location: string;
-	datetime: string;
-	supervisors: string[];
-	capacity: number;
-	image_src: string;
-	tags: string[] | undefined;
-};
-
-function formatDateTimeRange(startDateString: string, endDateString: string) {
-	const startDate = new Date(startDateString);
-	const endDate = new Date(endDateString);
-
-	const startDateFormatted = `${
-		startDate.getUTCMonth() + 1
-	}/${startDate.getUTCDate()}/${startDate.getUTCFullYear()}`;
-	const startTimeFormatted = formatUTCTime(startDate);
-	const endTimeFormatted = formatUTCTime(endDate);
-
-	const formattedDateTimeRange = `${startDateFormatted}, ${startTimeFormatted} - ${endTimeFormatted}`;
-
-	return formattedDateTimeRange;
-}
-
-function formatUTCTime(date: Date) {
-	const hours = date.getUTCHours();
-	const minutes = date.getUTCMinutes();
-
-	const period = hours < 12 ? "AM" : "PM";
-	const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-	const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-
-	return `${formattedHours}:${formattedMinutes} ${period}`;
-}
 
 /** An EventRegistration page */
 const EventRegistration = () => {
-	const router = useRouter();
-	const { eventid } = router.query;
-	const [eventDetails, setEventDetails] = useState<eventData | null>(null);
-	const [attendees, setAttendees] = useState<any[]>([]);
-	const [isRegistered, setIsRegistered] = useState<boolean>(false);
+  const router = useRouter();
+  const { eventid } = router.query;
 
-	const { user } = useAuth();
-	const url = BASE_URL as string;
-	useEffect(() => {
-		fetchEventDetails();
-	}, []);
-
-	const fetchEventDetails = async () => {
-		const userToken = await auth.currentUser?.getIdToken();
-		try {
-			const userId = await fetchUserIdFromDatabase(
-				user?.email as string,
-				userToken as string
-			);
-			const fetchUrl = `${url}/users/${userId}/registered?eventid=${eventid}`;
-			const response = await fetch(fetchUrl, {
-				method: "GET",
-				headers: {
-					Authorization: `Bearer ${userToken}`,
-				},
-			});
-
-			const data = await response.json();
-
-			const event = data["data"]["eventDetails"];
-			setEventDetails({
-				eventid: event["id"],
-				location: event["location"],
-				datetime: formatDateTimeRange(event["startDate"], event["endDate"]),
-				capacity: event["capacity"],
-				image_src: event["imageURL"],
-				tags: event["tags"],
-				supervisors: [
-					event["owner"]["profile"]["firstName"] +
-						" " +
-						event["owner"]["profile"]["lastName"],
-				],
-			});
-
-			if (
-				data["data"]["attendance"] &&
-				data["data"]["attendance"]["canceled"]
-			) {
-				router.push(`/events/${eventid}/cancel`);
-			} else if (data["data"]["attendance"]) {
-				setIsRegistered(true);
-			} else {
-				setIsRegistered(false);
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	return (
-		<CenteredTemplate>
-			{eventDetails ? (
-				// If attendees is empty -> return RegisterForm
-				!isRegistered ? (
-					<EventRegisterForm eventDetails={eventDetails} />
-				) : (
-					// If attendees is not empty (has 1 entry) -> return RegisterConfirmation
-					<EventConfirmation
-						eventDetails={eventDetails}
-						confirmation="register"
-					/>
-				)
-			) : (
-				<div>Getting your data...</div>
-			)}
-		</CenteredTemplate>
-	);
+  return (
+    <CenteredTemplate>
+      <EventRegisterForm
+        eventid={eventid as string}
+        location="Address, Building Name"
+        datetime="02/15/2023, 9:00AM-11:00AM"
+        supervisors={["Jane Doe", "Jess Lee"]}
+        capacity={20}
+        image_src="https://i0.wp.com/roadmap-tech.com/wp-content/uploads/2019/04/placeholder-image.jpg?resize=800%2C800&ssl=1"
+        tags={["In-person", "EDUFOOD"]}
+      />
+    </CenteredTemplate>
+  );
 };
 
 export default EventRegistration;
