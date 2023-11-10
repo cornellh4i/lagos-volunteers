2024c3655f7236638a244c86acbb6bcdf43b3185
Yichen Yao <yy486@cornell.edu>
2023-11-10 18:57:44 -0500

    debugged and finish locationpicker

--- frontend/src/components/atoms/LocationPicker.tsx
+++ frontend/src/components/atoms/LocationPicker.tsx
@@ -11,15 +11,20 @@ interface LocationPickerProps {
  */
 const LocationPicker = ({ label }: LocationPickerProps) => {
   return (
-    <div>
-      <div> {label} </div>
-      <Autocomplete
-        apiKey={process.env.GOOGLE_API_KEY}
-        onPlaceSelected={(place) => {
-          console.log(place);
-        }}
-      />
-    </div>
+      <div>
+        <Autocomplete
+          apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+          onPlaceSelected={(place) => {
+            console.log(place);
+          }}
+          className="box-border border border-solid rounded w-full p-2 text-base border-gray-400 hover:border-black"
+          options={{
+            fields: ["address_components"],
+            types: ["address"],
+          }}
+        />
+      </div>
+      
   );
 };
 
