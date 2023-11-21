7860126f98f74aca5cc08fbc1e56add24ef2e0e9
Sophie Wang <sophwang024@gmail.com>
2023-11-21 08:20:06 +0000

    Removed comments

--- frontend/src/components/organisms/About.tsx
+++ frontend/src/components/organisms/About.tsx
@@ -47,7 +47,10 @@ const About = ({ edit }: AboutProps) => {
   </p>
   <h2>Programs</h2>`;
   {
-    // <ViewEvents />;
+    <div>
+      <EventCard>hello</EventCard>
+    </div>;
+    //<ViewEvents />;
   }
   +`
   <h2>Certificate Request Process</h2>
@@ -101,113 +104,6 @@ const About = ({ edit }: AboutProps) => {
       </>
     );
   }
-  // if (edit == false) {
-  //   return (
-  //     <>
-  //       <div>
-  //         <h2>About Lagos Food Bank</h2>
-  //         <h3>Mission</h3>
-  //         <p>
-  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  //           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-  //           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-  //           aliquip ex ea commodo consequat. Duis aute irure dolor in
-  //           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-  //           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-  //           culpa qui officia deserunt mollit anim id est laborum.
-  //         </p>
-  //         <h3>Why Volunteer?</h3>
-  //         <p>
-  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  //           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-  //           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-  //           aliquip ex ea commodo consequat. Duis aute irure dolor in
-  //           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-  //           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-  //           culpa qui officia deserunt mollit anim id est laborum.
-  //         </p>
-  //         <h2>Sign Up Process</h2>
-  //         <p>
-  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  //           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-  //           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-  //           aliquip ex ea commodo consequat. Duis aute irure dolor in
-  //           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-  //           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-  //           culpa qui officia deserunt mollit anim id est laborum.
-  //         </p>
-  //         <h2>Programs</h2>
-  //         {/* <ViewEvents /> */}
-  //         <h2>Certificate Request Process</h2>
-  //         <p>
-  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  //           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-  //           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-  //           aliquip ex ea commodo consequat. Duis aute irure dolor in
-  //           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-  //           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-  //           culpa qui officia deserunt mollit anim id est laborum.
-  //         </p>
-  //         <h2>Request Certificate</h2>
-  //         <p>Fill out this form. Login first</p>
-  //       </div>
-  //     </>
-  //   );
-  // } else {
-  //   return (
-  //     <>
-  //       <div>
-  //         <h2>About Lagos Food Bank</h2>
-  //         <h3>Mission</h3>
-  //         <p>
-  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  //           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-  //           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-  //           aliquip ex ea commodo consequat. Duis aute irure dolor in
-  //           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-  //           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-  //           culpa qui officia deserunt mollit anim id est laborum.
-  //         </p>
-  //         <h3>Why Volunteer?</h3>
-  //         <p>
-  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  //           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-  //           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-  //           aliquip ex ea commodo consequat. Duis aute irure dolor in
-  //           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-  //           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-  //           culpa qui officia deserunt mollit anim id est laborum.
-  //         </p>
-  //         <h2>Sign Up Process</h2>
-  //         <p>
-  //           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-  //           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-  //           ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-  //           aliquip ex ea commodo consequat. Duis aute irure dolor in
-  //           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-  //           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-  //           culpa qui officia deserunt mollit anim id est laborum.
-  //         </p>
-  //         <link
-  //           href="https://cdn.quilljs.com/1.3.6/quill.snow.css"
-  //           rel="stylesheet"
-  //         />
-
-  //         <div id="editor">
-  //           <p>Hello World!</p>
-  //           <p>
-  //             Some initial <strong>bold</strong> text
-  //           </p>
-  //           <p>
-  //             <br />
-  //           </p>
-  //         </div>
-
-  //         <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
-  //       </div>
-  //     </>
-  //   );
-  // }
 };
 
 export default About;
