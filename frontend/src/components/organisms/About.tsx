4562dcef60af8eba33304dfcba87bb9468d8ac7a
Sneha Rajaraman <sr934@cornell.edu>
2023-11-30 16:03:52 -0500

    Removed cards, added useState functionality, added modal

--- frontend/src/components/organisms/About.tsx
+++ frontend/src/components/organisms/About.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Modal, Grid } from "@mui/material";
+import { Box, Button, Modal, Grid, Typography } from "@mui/material";
 import ViewEvents from "./ViewEvents";
 import React, { useState } from "react";
 import ReactQuill from "react-quill";
@@ -8,6 +8,7 @@ import { renderToStaticMarkup } from "react-dom/server";
 import EventCard from "./EventCard";
 import MultilineTextField from "../atoms/MultilineTextField";
 import CardList from "../molecules/CardList";
+import { handleSmoothScroll } from "next/dist/shared/lib/router/router";
 
 interface AboutProps {
   edit: boolean;
@@ -50,9 +51,7 @@ const About = ({ edit }: AboutProps) => {
     pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
     culpa qui officia deserunt mollit anim id est laborum.
   </p>
-  <h2>Programs</h2>`;
-  var button = <Button>hello</Button>;
-  var default_text_two = `
+  <h2>Programs</h2>
   <h2>Certificate Request Process</h2>
   <p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -66,82 +65,71 @@ const About = ({ edit }: AboutProps) => {
   <h2>Request Certificate</h2>
   <p>Fill out this form. Login first</p>
   `;
-  const [value, setValue] = useState(`${default_text} ${default_text_two}`);
+  const [value, setValue] = useState(`${default_text}`);
   const [open, setOpen] = React.useState(false);
+  const [editMode, setEditMode] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const handleClick = () => setEditMode(true);
+  const handleClickText = () => setEditMode(false);
+  const handleModalClick = () => {
+    handleClickText()
+    handleClose()
+  }
 
-  <Modal open={open} onClose={handleClose}>
-    <Box>Hello</Box>
-  </Modal>;
-  return (
-    <>
-      <ReactQuill
-        theme="snow"
-        value={value}
-        onChange={setValue}
-        readOnly={!edit}
-      />
-      <br></br>
-      <CardList>
-        <EventCard
-          eventid={"000"}
-          mainAction={"cancel rsvp"}
-          title={"EDUFOOD"}
-          location={"WAREHOUSE B"}
-          datetime={"02/15/2023, 9:00-11:00 AM"}
-          dropdownActions={[]}
+  if (editMode == true) {
+    const style = {
+      position: 'absolute' as 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: 400,
+      bgcolor: 'background.paper',
+      border: '2px solid #000',
+      boxShadow: 24,
+      p: 4,
+    };
+    return (
+      <>
+        < Modal open={open} onClose={handleModalClick} >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Are you sure you want to publish changes to this page?
+            </Typography>
+            <div className="text-center">
+              <button type="button" onClick={handleModalClick}>Yes, publish.</button>
+              <button className="ml-4" type="button" onClick={handleClose}>
+                No, cancel.
+              </button>
+            </div>
+          </Box>
+        </Modal >
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          // readOnly={!edit}
+          readOnly={false}
         />
-        <EventCard
-          eventid={"000"}
-          mainAction={"cancel rsvp"}
-          title={"Malta Outreach"}
-          location={"Plot 2, Lagos Food Bank Building"}
-          datetime={"02/16/2023, 9:00-11:00 AM"}
-          dropdownActions={[]}
-        />
-        <EventCard
-          eventid={"000"}
-          mainAction={"cancel rsvp"}
-          dropdownActions={["cancel rsvp"]}
-          title={"NUMEPLAN"}
-          location={"Plot 2, Lagos Food Bank Building"}
-          datetime={"02/19/2023, 9:00-11:00 AM"}
-        />
-        <EventCard
-          eventid={"000"}
-          mainAction={"cancel rsvp"}
-          title={"EDUFOOD"}
-          location={"WAREHOUSE B"}
-          datetime={"02/15/2023, 9:00-11:00 AM"}
-          dropdownActions={[]}
-        />
-        <EventCard
-          eventid={"000"}
-          mainAction={"cancel rsvp"}
-          title={"Malta Outreach"}
-          location={"Plot 2, Lagos Food Bank Building"}
-          datetime={"02/16/2023, 9:00-11:00 AM"}
-          dropdownActions={[]}
-        />
-        <EventCard
-          eventid={"000"}
-          mainAction={"cancel rsvp"}
-          dropdownActions={["cancel rsvp"]}
-          title={"NUMEPLAN"}
-          location={"Plot 2, Lagos Food Bank Building"}
-          datetime={"02/19/2023, 9:00-11:00 AM"}
-        />
-      </CardList>
+        <br></br>
 
-      <div className="text-right">
-        <button type="button">Cancel</button>
-        <button className="ml-4" type="button" onClick={handleOpen}>
-          Publish Changes
-        </button>
-      </div>
-    </>
-  );
+        <div className="text-right">
+          <button type="button" disabled>Cancel</button>
+          <button className="ml-4" type="button" onClick={handleOpen}>
+            Publish Changes
+          </button>
+        </div>
+      </>
+    );
+  }
+  else {
+    return (
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: value }} />
+        <button type="button" onClick={handleClick}>Edit</button>
+      </div >
+    );
+  }
 };
 
 export default About;
