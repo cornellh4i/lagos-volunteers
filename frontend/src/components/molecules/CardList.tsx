27493ba9802f04f284beff813913072a07cce248
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2023-12-02 14:44:07 -0500

    Attempt 2 to remove uncessary files

---
+++ frontend/src/components/molecules/CardList.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from "react";
+
+interface CardListProps {
+  children: ReactElement[];
+}
+
+/**
+ * A CardList component is a responsive list of cards that wraps all the cards
+ * contained within
+ */
+const CardList = ({ children }: CardListProps) => {
+  return (
+    <>
+      {/* Show only on large screens */}
+      <div className="hidden sm:flex flex-wrap pt-3">
+        {children.map((card) => {
+          return <div className="pr-5 pb-5 w-96">{card}</div>;
+        })}
+      </div>
+
+      {/* Show only on small screens */}
+      <div className="sm:hidden flex flex-col pt-3">
+        {children.map((card) => {
+          return <div className="pb-4">{card}</div>;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default CardList;
---
+++ frontend/src/components/molecules/Modal.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Modal, Grid, Backdrop, Fade, Box, Typography } from "@mui/material";
+
+interface CustomModalProps {
+  open: boolean;
+  handleClose: () => void;
+  children: React.ReactElement;
+}
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  bgcolor: "background.paper",
+  borderRadius: "1rem",
+  fontweight: "bold",
+  boxShadow: 4,
+};
+
+const styleSmall = {
+  width: "80%",
+  padding: 4,
+  display: { xs: "block", md: "none" },
+};
+
+const styleLarge = {
+  width: 500,
+  padding: "4rem 6rem",
+  display: { xs: "none", md: "block" },
+};
+
+/**
+ * A Modal component is a styled modal that takes in a body component
+ */
+const CustomModal = ({ open, handleClose, children }: CustomModalProps) => {
+  return (
+    <Modal
+      className="w-full"
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <div>
+        <Box sx={[style, styleSmall]}>{children}</Box>
+        <Box sx={[style, styleLarge]}>{children}</Box>
+      </div>
+    </Modal>
+  );
+};
+
+export default CustomModal;
