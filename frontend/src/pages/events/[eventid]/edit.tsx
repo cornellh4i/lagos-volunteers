0c18149c7d5f023019e1a0699a59d9c9a701f84b
Jason Zheng <jasonz4200@gmail.com>
2023-12-13 18:50:58 -0500

    Format with Prettier

--- frontend/src/pages/events/[eventid]/edit.tsx
+++ frontend/src/pages/events/[eventid]/edit.tsx
@@ -7,74 +7,74 @@ import { BASE_URL } from "@/utils/constants";
 import Loading from "@/components/molecules/Loading";
 
 type eventData = {
-	eventName: string;
-	location: string;
-	volunteerSignUpCap: string;
-	eventDescription: string;
-	eventImage: string;
-	rsvpLinkImage: string;
-	startDate: string;
-	endDate: string;
-	startTime: string;
-	endTime: string;
-	mode: string;
+  eventName: string;
+  location: string;
+  volunteerSignUpCap: string;
+  eventDescription: string;
+  eventImage: string;
+  rsvpLinkImage: string;
+  startDate: string;
+  endDate: string;
+  startTime: string;
+  endTime: string;
+  mode: string;
 };
 
 /** An EditEvent page */
 const EditEvent = () => {
-	const router = useRouter();
-	const { eventid } = router.query;
-	const [eventDetails, setEventDetails] = useState<
-		eventData | null | undefined
-	>(null);
+  const router = useRouter();
+  const { eventid } = router.query;
+  const [eventDetails, setEventDetails] = useState<
+    eventData | null | undefined
+  >(null);
 
-	const fetchEventDetails = async () => {
-		try {
-			const url = BASE_URL as string;
-			const fetchUrl = `${url}/events/${eventid}`;
-			const userToken = await auth.currentUser?.getIdToken();
-			const response = await fetch(fetchUrl, {
-				method: "GET",
-				headers: {
-					Authorization: `Bearer ${userToken}`,
-				},
-			});
-			const data = await response.json();
-			setEventDetails({
-				eventName: data["data"]["name"],
-				location: data["data"]["location"],
-				volunteerSignUpCap: data["data"]["capacity"],
-				eventDescription: data["data"]["description"],
-				eventImage: data["data"]["eventImage"] || "",
-				rsvpLinkImage: data["data"]["rsvpLinkImage"] || "",
-				startDate: data["data"]["startDate"],
-				endDate: data["data"]["endDate"],
-				startTime: data["data"]["startDate"],
-				endTime: data["data"]["endDate"],
-				mode: data["data"]["mode"],
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const fetchEventDetails = async () => {
+    try {
+      const url = BASE_URL as string;
+      const fetchUrl = `${url}/events/${eventid}`;
+      const userToken = await auth.currentUser?.getIdToken();
+      const response = await fetch(fetchUrl, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+      const data = await response.json();
+      setEventDetails({
+        eventName: data["data"]["name"],
+        location: data["data"]["location"],
+        volunteerSignUpCap: data["data"]["capacity"],
+        eventDescription: data["data"]["description"],
+        eventImage: data["data"]["eventImage"] || "",
+        rsvpLinkImage: data["data"]["rsvpLinkImage"] || "",
+        startDate: data["data"]["startDate"],
+        endDate: data["data"]["endDate"],
+        startTime: data["data"]["startDate"],
+        endTime: data["data"]["endDate"],
+        mode: data["data"]["mode"],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	useEffect(() => {
-		fetchEventDetails();
-	}, []);
+  useEffect(() => {
+    fetchEventDetails();
+  }, []);
 
-	return (
-		<CenteredTemplate>
-			{eventDetails ? (
-				<EventForm
-					eventId={eventid}
-					eventType="edit"
-					eventDetails={eventDetails}
-				/>
-			) : (
-				<Loading />
-			)}
-		</CenteredTemplate>
-	);
+  return (
+    <CenteredTemplate>
+      {eventDetails ? (
+        <EventForm
+          eventId={eventid}
+          eventType="edit"
+          eventDetails={eventDetails}
+        />
+      ) : (
+        <Loading />
+      )}
+    </CenteredTemplate>
+  );
 };
 
 export default EditEvent;
--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -9,54 +9,54 @@ in the database
 @param token: string
 */
 export const fetchUserIdFromDatabase = async (email: string, token: string) => {
-	try {
-		const fetchUrl = `${url}/users/search/?email=${email}`;
-		const response = await fetch(fetchUrl, {
-			method: "GET",
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		});
-		if (response.ok) {
-			const data = await response.json();
-			return data["data"][0]["id"];
-		} else {
-			console.error("User Retrieval failed with status:", response.status);
-		}
-	} catch (error) {
-		console.error("Error in User Info Retrieval.");
-	}
+  try {
+    const fetchUrl = `${url}/users/search/?email=${email}`;
+    const response = await fetch(fetchUrl, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (response.ok) {
+      const data = await response.json();
+      return data["data"][0]["id"];
+    } else {
+      console.error("User Retrieval failed with status:", response.status);
+    }
+  } catch (error) {
+    console.error("Error in User Info Retrieval.");
+  }
 };
 
 /* This functions formats 2 date strings in the format: 00:00 AM - 00:00 PM
 @param startDateString: string
 @param endDateString: string
 */
-export const formatDateTimeRange = (startDateString: string, endDateString: string) => {
+export const formatDateTimeRange = (
+  startDateString: string,
+  endDateString: string
+) => {
+  const formatUTCTime = (date: Date) => {
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
 
-	const formatUTCTime = (date: Date) => {
-		const hours = date.getUTCHours();
-		const minutes = date.getUTCMinutes();
+    const period = hours < 12 ? "AM" : "PM";
+    const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
 
-		const period = hours < 12 ? "AM" : "PM";
-		const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-		const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${formattedHours}:${formattedMinutes} ${period}`;
+  };
 
-		return `${formattedHours}:${formattedMinutes} ${period}`;
-	}
+  const startDate = new Date(startDateString);
+  const endDate = new Date(endDateString);
 
-	const startDate = new Date(startDateString);
-	const endDate = new Date(endDateString);
+  const startDateFormatted = `${
+    startDate.getUTCMonth() + 1
+  }/${startDate.getUTCDate()}/${startDate.getUTCFullYear()}`;
+  const startTimeFormatted = formatUTCTime(startDate);
+  const endTimeFormatted = formatUTCTime(endDate);
 
-	const startDateFormatted = `${startDate.getUTCMonth() + 1
-		}/${startDate.getUTCDate()}/${startDate.getUTCFullYear()}`;
-	const startTimeFormatted = formatUTCTime(startDate);
-	const endTimeFormatted = formatUTCTime(endDate);
+  const formattedDateTimeRange = `${startDateFormatted}, ${startTimeFormatted} - ${endTimeFormatted}`;
 
-	const formattedDateTimeRange = `${startDateFormatted}, ${startTimeFormatted} - ${endTimeFormatted}`;
-
-	return formattedDateTimeRange;
-}
-
-
-
+  return formattedDateTimeRange;
+};
