a4877ff902d7adc9f21e46ff692af140278c185d
Jason Zheng <jasonz4200@gmail.com>
2023-12-13 18:51:49 -0500

    Format with Prettier

--- frontend/src/pages/users/[userid]/manage.tsx
+++ frontend/src/pages/users/[userid]/manage.tsx
@@ -4,11 +4,11 @@ import DefaultTemplate from "@/components/templates/DefaultTemplate";
 
 /** A Manage User Profile page */
 const ManageUserProfilePage = () => {
-	return (
-		<DefaultTemplate>
-			<ManageUserProfile />
-		</DefaultTemplate>
-	);
+  return (
+    <DefaultTemplate>
+      <ManageUserProfile />
+    </DefaultTemplate>
+  );
 };
 
 export default ManageUserProfilePage;
