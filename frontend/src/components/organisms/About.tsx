5dc784452d2be800121e9431d6bbd96150ae3d11
snehar27 <81458184+snehar27@users.noreply.github.com>
2023-12-19 19:49:44 -0500

    About page frontend (#131)
    * Completed ManageUsers
    
    * Bolded headers and fixed spacing
    
    * Implement ViewEvents.tsx
    
    * Implement ViewEvents.tsx (fullstack)
    
    * initial commit
    
    * Implemented About.tsx
    
    * Imported ReactQuill
    
    * no ssr
    
    * Set up default value
    
    * Set default value
    
    * Updated default value
    
    * Updated cards
    
    * added buttons
    
    * Removed comments
    
    * about page
    
    * Fixed edit/readOnly component
    
    * Added JSX component
    
    * Added Cards
    
    * cards
    
    * Reformat Code
    
    * second attempt to remove unecessaty files
    
    * Removed cards, added useState functionality, added modal
    
    * Reimplemented modal
    
    * Changed implementation for parsing string
    
    * Attempt to remove unnecessary files
    
    * Attempt 2 to remove uncessary files
    
    * Fix package.json formatting
    
    * Add AboutPage
    
    * Add react-html-parser dependency
    
    * Format with Prettier
    
    * Minor refactoring
    
    ---------
    
    Co-authored-by: Louis Valencia <lcv28@cornell.edu>
    Co-authored-by: Sneha Rajaraman <sr934@cornell.edu>
    Co-authored-by: Sophie Wang <sophwang024@gmail.com>
    Co-authored-by: Akinfolami Akin-Alamu <aoa9@cornell.edu>
    Co-authored-by: Jason Zheng <jasonz4200@gmail.com>

--- frontend/src/components/organisms/About.tsx
+++ frontend/src/components/organisms/About.tsx
@@ -1,18 +1,151 @@
-import React from "react";
+import { Grid } from "@mui/material";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import Button from "../atoms/Button";
+import Modal from "../molecules/Modal";
+import ReactHtmlParser from "react-html-parser";
 
 interface AboutProps {
   edit: boolean;
 }
 
+type modalBodyProps = {
+  handleModal: () => void;
+  handleClose: () => void;
+};
+
+const ModalBody = ({ handleModal, handleClose }: modalBodyProps) => {
+  return (
+    <div>
+      <p>Are you sure you want to publish changes to this page?</p>
+      <Grid container spacing={2}>
+        <Grid item md={6} xs={12}>
+          <Button color="gray" onClick={handleModal}>
+            Yes, publish
+          </Button>
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <Button color="dark-gray" onClick={handleClose}>
+            No, cancel
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
 /**
  * An About component
  */
 const About = ({ edit }: AboutProps) => {
-  return (
-    <>
-      <div>Lorem</div>
-    </>
-  );
+  var default_text = `
+  <h2>About Lagos Food Bank</h2>
+  <h3>Mission</h3>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <h3>Why Volunteer?</h3>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <h2>Sign Up Process</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <h2>Programs</h2>
+  <h2>Certificate Request Process</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+    aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  <h2>Request Certificate</h2>
+  <p>Fill out this form. Login first</p>
+  `;
+  const [value, setValue] = useState(`${default_text}`);
+  const [open, setOpen] = React.useState(false);
+  const [editMode, setEditMode] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleClick = () => setEditMode(true);
+  const handleModalClick = () => {
+    setEditMode(false);
+    handleClose();
+  };
+
+  if (editMode == true) {
+    return (
+      <>
+        <div className="space-y-2">
+          <Modal
+            open={open}
+            handleClose={handleClose}
+            children={
+              <ModalBody
+                handleModal={handleModalClick}
+                handleClose={handleClose}
+              />
+            }
+          />
+        </div>
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          readOnly={false}
+        />
+        <br></br>
+
+        <div className="text-right">
+          <Grid item container>
+            <Grid xs={10}></Grid>
+            <Grid xs={2}>
+              <Button color={"gray"} onClick={handleOpen}>
+                Publish Changes
+              </Button>
+            </Grid>
+          </Grid>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <div>{ReactHtmlParser(value)}</div>
+        <Grid item container>
+          <Grid xs={11}></Grid>
+          <Grid xs={1}>
+            <Button color={"gray"} onClick={handleClick}>
+              Edit
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 };
 
 export default About;
