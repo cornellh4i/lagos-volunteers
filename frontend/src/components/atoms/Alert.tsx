e9607612d416dfb9cfe50d9b108910ddd63c3f99
Jason Zheng <jasonz4200@gmail.com>
2023-12-21 19:04:49 -0500

    Implement Alert atom

--- frontend/src/components/atoms/Alert.tsx
+++ frontend/src/components/atoms/Alert.tsx
@@ -1,14 +1,46 @@
 import React, { ReactNode } from "react";
 import MuiAlert from "@mui/material/Alert";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 interface AlertProps {
   children: ReactNode;
+  variety: "success" | "error";
   [key: string]: any;
 }
 
 /** A simple Alert component */
-const Alert = ({ children, ...props }: AlertProps) => {
-  return <MuiAlert {...props}>{children}</MuiAlert>;
+const Alert = ({ children, variety, ...props }: AlertProps) => {
+  // Set alert variety
+  let bgcolor = "";
+  let icon;
+  switch (variety) {
+    case "success":
+      bgcolor = "primary.light";
+      icon = <CheckCircleIcon color="success" fontSize="inherit" />;
+      break;
+    case "error":
+      bgcolor = "error.light";
+      icon = <CancelIcon color="error" fontSize="inherit" />;
+      break;
+  }
+
+  return (
+    <MuiAlert
+      onClose={() => {}}
+      variant="filled"
+      icon={icon}
+      sx={{
+        bgcolor: bgcolor,
+        color: "black",
+        borderRadius: "8px",
+        padding: "16px 32px",
+      }}
+      {...props}
+    >
+      {children}
+    </MuiAlert>
+  );
 };
 
 export default Alert;
