caf14ff207b26a08b7e36895ee5a8ead55599c36
Jason Zheng <jasonz4200@gmail.com>
2023-12-23 01:58:03 -0500

    Refactor Avatar

--- frontend/src/components/molecules/Avatar.tsx
+++ frontend/src/components/molecules/Avatar.tsx
@@ -1,41 +1,33 @@
 import React from "react";
+import MuiAvatar from "@mui/material/Avatar";
+import PersonIcon from "@mui/icons-material/Person";
 
-/** default royalty free avatar image */
-export const FALLBACK_AVATAR_URL =
-  "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
-export const FALLBACK_AVATOR_ALT = "avatar";
-
-/** both the image url and alt text are optional */
 interface AvatarProps {
-  url?: string;
-  alt?: string;
-  name: string | undefined;
-  start_date: Date;
-  hour: number;
+  name: string;
+  image?: string;
+  startDate: Date;
+  email?: string;
+  phone?: string;
 }
 
-const Avatar = ({
-  url = FALLBACK_AVATAR_URL,
-  alt = FALLBACK_AVATOR_ALT,
-  name,
-  start_date,
-  hour,
-}: AvatarProps) => {
+const Avatar = ({ image, name, startDate, email, phone }: AvatarProps) => {
   return (
     <div className="sm:flex">
-      <div className="flex justify-center">
-        <img
-          src={url || FALLBACK_AVATAR_URL}
-          alt={alt}
-          className="rounded-full w-32"
-        />
+      <div className="flex justify-center my-auto">
+        <MuiAvatar
+          src={image}
+          sx={{ height: "125px", width: "125px", fontSize: "75px" }}
+        >
+          <PersonIcon fontSize="inherit" />
+        </MuiAvatar>
       </div>
-      <div className="p-6 font-sans text-center sm:text-left">
-        <div className="text-3xl font-semibold">{name}</div>
-        <div className="py-1">
-          Volunteer since {start_date.toLocaleDateString("en-US")}
+      <div className="p-6 text-center sm:text-left">
+        <div className="text-2xl font-semibold">{name}</div>
+        <div className="pt-2">
+          Joined {startDate.toLocaleDateString("en-US")}
         </div>
-        <div>{hour} volunteer hours completed</div>
+        <div className="pt-1">{email}</div>
+        <div className="pt-1">{phone}</div>
       </div>
     </div>
   );
