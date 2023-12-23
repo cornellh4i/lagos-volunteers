a541a68e47e64234d49946c42323e72fb394340a
Jason Zheng <jasonz4200@gmail.com>
2023-12-22 19:27:23 -0500

    Refactor datepicker

--- frontend/src/components/atoms/DatePicker.tsx
+++ frontend/src/components/atoms/DatePicker.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef, Ref } from "react";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
@@ -15,38 +15,38 @@ interface DatePickerProps {
  * A DatePicker component is an input field that allows selecting a specific
  * date through a calendar popup
  */
-const CustomDatePicker = ({
-  label,
-  error,
-  value,
-  ...props
-}: DatePickerProps) => {
-  return (
-    <div>
-      <div className="mb-1">{label}</div>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          sx={{
-            "& .MuiInputBase-root": {
-              borderRadius: "8px",
-            },
-            "& .MuiInputBase-input": {
-              height: "12px",
-            },
-          }}
-          slotProps={{
-            textField: { fullWidth: true, error: error !== "" },
-          }}
-          label=""
-          format="MM/DD/YYYY"
-          defaultValue={value ? dayjs(value) : undefined}
-          disablePast={true}
-          {...props}
-        />
-      </LocalizationProvider>
-      <div className="mt-1 text-xs text-red-500">{error}</div>
-    </div>
-  );
-};
+const CustomDatePicker = forwardRef(
+  (
+    { label, error, value, ...props }: DatePickerProps,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    return (
+      <div>
+        <div className="mb-1">{label}</div>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            sx={{
+              "& .MuiInputBase-root": {
+                borderRadius: "8px",
+              },
+              "& .MuiInputBase-input": {
+                height: "12px",
+              },
+            }}
+            slotProps={{
+              textField: { fullWidth: true, error: error !== "" },
+            }}
+            ref={ref}
+            format="MM/DD/YYYY"
+            defaultValue={value ? dayjs(value) : undefined}
+            disablePast={true}
+            {...props}
+          />
+        </LocalizationProvider>
+        <div className="mt-1 text-xs text-red-500">{error}</div>
+      </div>
+    );
+  }
+);
 
 export default CustomDatePicker;
