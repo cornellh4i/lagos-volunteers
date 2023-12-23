baa9e64fd4938f2c66186ef68821739854e49933
Jason Zheng <jasonz4200@gmail.com>
2023-12-23 16:19:25 -0500

    Refactor Manage Attendees

--- frontend/src/components/organisms/ManageAttendees.tsx
+++ frontend/src/components/organisms/ManageAttendees.tsx
@@ -1,31 +1,20 @@
 import React, { ChangeEvent, FormEvent } from "react";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import TabContainer from "@/components/molecules/TabContainer";
 import { GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 import Table from "@/components/molecules/Table";
+import { MenuItem } from "@mui/material";
 import Button from "../atoms/Button";
-import Link from "next/link";
-import CheckBox from "@/components/atoms/Checkbox";
 import SearchBar from "../atoms/SearchBar";
-import IconText from "../atoms/IconText";
+import Select from "../atoms/Select";
 
 interface ManageAttendeesProps {}
 
-const eventColumnsPending: GridColDef[] = [
-  {
-    field: "check",
-    headerName: "",
-    maxWidth: 50,
-    flex: 0.5,
-    renderCell: (params) => {
-      return <CheckBox label={params.value} />;
-    },
-  },
+const eventColumns: GridColDef[] = [
   {
     field: "name",
     headerName: "Name",
-    minWidth: 100,
-    flex: 0.5,
+    minWidth: 200,
+    flex: 2,
     renderHeader: (params) => (
       <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
     ),
@@ -33,123 +22,46 @@ const eventColumnsPending: GridColDef[] = [
   {
     field: "email",
     headerName: "Email",
-    minWidth: 100,
-    flex: 0.5,
+    minWidth: 200,
+    // flex: 0.5,
     renderHeader: (params) => (
       <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
     ),
   },
   {
-    field: "role",
-    headerName: "Role",
-    minWidth: 100,
-    flex: 2.5,
+    field: "phone",
+    headerName: "Phone number",
+    minWidth: 200,
+    // flex: 0.5,
     renderHeader: (params) => (
       <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
     ),
   },
   {
-    field: "button",
-    headerName: "",
-    minWidth: 100,
-    flex: 0.5,
+    field: "status",
+    headerName: "Status",
+    minWidth: 175,
+    // flex: 0.5,
+    renderHeader: (params) => (
+      <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
+    ),
     renderCell: () => (
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <Button children="Check In"></Button>
+      <div className="w-full">
+        <Select
+          value="PENDING"
+          // value={role}
+          onChange={(event: any) => console.log(event.target.value)}
+        >
+          <MenuItem value="CHECKED IN">Checked in</MenuItem>
+          <MenuItem value="CHECKED OUT">Checked out</MenuItem>
+          <MenuItem value="PENDING">Pending</MenuItem>
+          <MenuItem value="REMOVED">Removed</MenuItem>
+        </Select>
       </div>
     ),
   },
 ];
-const eventColumnsCheckedIn: GridColDef[] = [
-  {
-    field: "check",
-    headerName: "",
-    maxWidth: 50,
-    flex: 0.5,
-    renderCell: (params) => {
-      return <CheckBox label={params.value} />;
-    },
-  },
-  {
-    field: "name",
-    headerName: "Name",
-    minWidth: 100,
-    flex: 0.5,
-    renderHeader: (params) => (
-      <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
-    ),
-  },
-  {
-    field: "email",
-    headerName: "Email",
-    minWidth: 100,
-    flex: 0.5,
-    renderHeader: (params) => (
-      <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
-    ),
-  },
-  {
-    field: "role",
-    headerName: "Role",
-    minWidth: 100,
-    flex: 2.5,
-    renderHeader: (params) => (
-      <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
-    ),
-  },
-  {
-    field: "button",
-    headerName: "",
-    minWidth: 100,
-    flex: 0.5,
-    renderCell: () => (
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <Button children="Check Out"></Button>
-      </div>
-    ),
-  },
-];
-const eventColumnsCheckedOut: GridColDef[] = [
-  {
-    field: "check",
-    headerName: "",
-    maxWidth: 50,
-    flex: 0.5,
-    renderHeader: (params) => (
-      <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
-    ),
-    renderCell: (params) => {
-      return <CheckBox label={params.value} />;
-    },
-  },
-  {
-    field: "name",
-    headerName: "Name",
-    minWidth: 100,
-    flex: 0.5,
-    renderHeader: (params) => (
-      <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
-    ),
-  },
-  {
-    field: "email",
-    headerName: "Email",
-    minWidth: 100,
-    flex: 0.5,
-    renderHeader: (params) => (
-      <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
-    ),
-  },
-  {
-    field: "role",
-    headerName: "Role",
-    minWidth: 100,
-    flex: 2.5,
-    renderHeader: (params) => (
-      <div style={{ fontWeight: "bold" }}>{params.colDef.headerName}</div>
-    ),
-  },
-];
+
 // below are dummy data, in the future we want to get data from backend and
 // format them like this
 let dummyDate: Date = new Date(2023, 0o1, 21);
@@ -159,70 +71,48 @@ const dummyRows = [
     check: false,
     name: "Greatest Ball Handler",
     email: "gbh@gmail.com",
-    role: "Volunteer",
+    phone: "123-456-7890",
   },
   {
     id: 2,
     check: false,
     name: "Big Ballllahh",
     email: "gbh@gmail.com",
-    role: "Volunteer",
+    phone: "123-456-7890",
   },
   {
     id: 3,
     check: false,
     name: "Top Shottaahh",
     email: "gbh@gmail.com",
-    role: "Volunteer",
+    phone: "123-456-7890",
   },
   {
     id: 4,
     check: false,
     name: "Brita Filter",
     email: "gbh@gmail.com",
-    role: "Volunteer",
+    phone: "123-456-7890",
   },
   {
     id: 5,
     check: false,
     name: "Ollie The Otter",
     email: "gbh@gmail.com",
-    role: "Volunteer",
+    phone: "123-456-7890",
   },
 ];
 
-const Pending = () => {
-  return (
-    <div>
-      <Table columns={eventColumnsPending} rows={dummyRows} />
-    </div>
-  );
-};
-const CheckedIn = () => {
-  return (
-    <div>
-      <Table columns={eventColumnsCheckedIn} rows={dummyRows} />
-    </div>
-  );
-};
-const CheckedOut = () => {
-  return (
-    <div>
-      <Table columns={eventColumnsCheckedOut} rows={dummyRows} />
-    </div>
-  );
-};
-
-/**
- * An ManageAttendees component
- */
-const ManageAttendees = ({}: ManageAttendeesProps) => {
-  const tabs = [
-    { label: "Pending", panel: <Pending /> },
-    { label: "Checked In", panel: <CheckedIn /> },
-    { label: "Checked Out", panel: <CheckedOut /> },
-  ];
-
+const AttendeesTable = ({
+  status,
+}: {
+  status:
+    | "pending"
+    | "checked in"
+    | "checked out"
+    | "registration removed"
+    | "canceled registration";
+}) => {
   // Search bar
   const [value, setValue] = React.useState("");
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -237,18 +127,8 @@ const ManageAttendees = ({}: ManageAttendeesProps) => {
   };
 
   return (
-    <>
-      <div className="flex items-center text-gray-400">
-        <IconText icon={<ArrowBackIcon />}>
-          <Link href="/events/view/" className="text-gray-400">
-            <u>Return to My Events</u>
-          </Link>
-        </IconText>
-      </div>
-      <div className="font-semibold text-3xl pt-5">
-        Manage Malta Outreach Volunteers
-      </div>
-      <div className="pt-5 pb-5 w-full sm:w-[600px]">
+    <div>
+      <div className="pb-5 w-full sm:w-[600px]">
         <SearchBar
           placeholder="Search member by name, email"
           value={value}
@@ -256,9 +136,38 @@ const ManageAttendees = ({}: ManageAttendeesProps) => {
           onClick={handleSubmit}
         />
       </div>
+      <Table columns={eventColumns} rows={dummyRows} />
+    </div>
+  );
+};
 
+/**
+ * An ManageAttendees component
+ */
+const ManageAttendees = ({}: ManageAttendeesProps) => {
+  const tabs = [
+    { label: "Pending", panel: <AttendeesTable status="pending" /> },
+    { label: "Checked in", panel: <AttendeesTable status="checked in" /> },
+    { label: "Checked out", panel: <AttendeesTable status="checked out" /> },
+    {
+      label: "Registration removed",
+      panel: <AttendeesTable status="registration removed" />,
+    },
+    {
+      label: "Canceled registration",
+      panel: <AttendeesTable status="canceled registration" />,
+    },
+  ];
+
+  return (
+    <>
+      <div className="font-semibold text-3xl mb-6">Malta Outreach</div>
+      <div className="font-semibold text-2xl mb-6">Event Recap</div>
+      <div>Event recap here</div>
+      <br />
+      <div className="font-semibold text-2xl mb-6">Manage Volunteers</div>
       <div>
-        <TabContainer tabs={tabs} />
+        <TabContainer fullWidth tabs={tabs} />
       </div>
     </>
   );
