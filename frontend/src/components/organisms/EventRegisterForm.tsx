4e01e2fea26f283e9da7d851c6140d835be6a359
Jason Zheng <jasonz4200@gmail.com>
2024-01-16 15:33:10 -0600

    fix

--- frontend/src/components/organisms/EventRegisterForm.tsx
+++ frontend/src/components/organisms/EventRegisterForm.tsx
@@ -6,7 +6,7 @@ import Button from "../atoms/Button";
 import Modal from "@/components/molecules/Modal";
 import Link from "next/link";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import { useRouter } from "next/router";
 import { useAuth } from "@/utils/AuthContext";
 import { fetchUserIdFromDatabase } from "@/utils/helpers";
@@ -18,9 +18,7 @@ interface EventRegisterFormProps {
   event: EventData;
 }
 
-/**
- * A confirmation modal body
- */
+/** A confirmation modal body */
 const ModalBody = ({
   eventid,
   handleClose,
@@ -28,13 +26,10 @@ const ModalBody = ({
   eventid: string;
   handleClose: () => void;
 }) => {
-  const router = useRouter();
   const { user } = useAuth();
   const queryClient = useQueryClient();
 
-  /**
-   * Handles clicking the Register button
-   */
+  /** Handles clicking the Register button */
   const { mutate, isPending, isError } = useMutation({
     mutationKey: ["event", eventid],
     mutationFn: async () => {
@@ -51,39 +46,37 @@ const ModalBody = ({
 
   return (
     <div>
-      <h2>Terms and Conditions</h2>
-      <p>
+      <Box sx={{ textAlign: "center" }}>
+        <h2>Terms and Conditions</h2>
+      </Box>
+      <Box sx={{ marginBottom: 3 }}>
         By registering, I agree that I will attend the event. If I cannot
         attend, I will cancel my registration at least 24 hours before the event
         begins. Failure to cancel my registration may negatively impact my
         status as a volunteer
-      </p>
+      </Box>
       <Grid container spacing={2}>
         <Grid item md={6} xs={12}>
-          <Button type="button" onClick={handleClose}>
-            Disagree
+          <Button variety="secondary" onClick={handleClose}>
+            Decline
           </Button>
         </Grid>
         <Grid item md={6} xs={12}>
-          <Button type="button" onClick={mutate}>
-            Agree & Continue
-          </Button>
+          <Button onClick={mutate}>Agree and register</Button>
         </Grid>
       </Grid>
     </div>
   );
 };
 
-/**
- * An EventRegisterForm component
- */
+/** An EventRegisterForm component */
 const EventRegisterForm = ({ event }: EventRegisterFormProps) => {
   const [checked, setChecked] = useState(false);
   const handleChange = () => {
     setChecked((checked) => !checked);
   };
 
-  // Confirmation modal
+  /** Confirmation modal */
   const [open, setOpen] = useState(false);
   const handleOpen = () => {
     if (checked) {
@@ -94,6 +87,7 @@ const EventRegisterForm = ({ event }: EventRegisterFormProps) => {
 
   return (
     <div>
+      {/* Confirmation modal */}
       <Modal
         open={open}
         handleClose={handleClose}
@@ -102,6 +96,7 @@ const EventRegisterForm = ({ event }: EventRegisterFormProps) => {
         }
       />
 
+      {/* Register form */}
       <div className="justify-center center-items grid grid-cols-4 grid-rows-6`">
         <div className="space-y-4 col-start-1 col-end-5">
           <div className="flex items-center text-gray-400">
