16f0b220e540a64bd4a7191d9ee481fa6c066bb6
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2024-02-10 19:27:57 -0500

    Make Avatar use names instead

--- frontend/src/components/molecules/Avatar.tsx
+++ frontend/src/components/molecules/Avatar.tsx
@@ -10,16 +10,43 @@ interface AvatarProps {
   phone?: string;
 }
 
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name: string) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+      height: "125px",
+      width: "125px",
+      fontSize: "75px",
+    },
+    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+  };
+}
+
 const Avatar = ({ image, name, startDate, email, phone }: AvatarProps) => {
   return (
     <div className="sm:flex">
       <div className="flex justify-center my-auto">
-        <MuiAvatar
-          src={image}
-          sx={{ height: "125px", width: "125px", fontSize: "75px" }}
-        >
-          <PersonIcon fontSize="inherit" />
-        </MuiAvatar>
+        <MuiAvatar src={image} {...stringAvatar(name)}></MuiAvatar>
       </div>
       <div className="p-6 text-center sm:text-left">
         <div className="text-2xl font-semibold">{name}</div>
