e82503381a5e484d74e72211d8bc9ad479df10cd
Owen Chen <owenchen502@gmail.com>
2024-02-17 04:50:24 +0000

    A tomcat (male cat) can begin mating when he is between 7 and 10 months old.

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -3,9 +3,22 @@ import { useRouter } from "next/router";
 
 /** A User page */
 const User = () => {
+  const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(response.json());
+  };
+
+  const handleClick = () => {
+    getCatFact();
+  };
+
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+  return (
+    <>
+      Hello user {userid}!<button onClick={handleClick}>cat fact</button>
+    </>
+  );
 };
 
 export default User;
