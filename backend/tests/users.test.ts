97f739c2a8976d5a335067d603ee6691a9d858bb
Trung-Nghia Le <ltn090403@gmail.com>
2024-02-18 04:59:35 +0000

    Fix Finished Swagger + Jest onboarding

--- backend/tests/users.test.ts
+++ backend/tests/users.test.ts
@@ -15,30 +15,30 @@ afterAll(async () => {
 });
 
 describe("Testing user Endpoints", () => {
-  // test("Get all users in DB", async () => {
-  //   const response = await request(app).get("/users");
-  //   expect(response.status).toBe(200);
-  //   let users = Array.isArray(response.body) ? response.body : [];
-  //   users.forEach((user) => {
-  //     expect(user).toHaveProperty("id");
-  //     expect(user).toHaveProperty("email");
-  //     expect(user).toHaveProperty("name");
+  test("Get all users in DB", async () => {
+    const response = await request(app).get("/users");
+    expect(response.status).toBe(200);
+    let users = Array.isArray(response.body) ? response.body : [];
+    users.forEach((user) => {
+      expect(user).toHaveProperty("id");
+      expect(user).toHaveProperty("email");
+      expect(user).toHaveProperty("name");
 
-  //     if (user.profile) {
-  //       // Check if the profile exists and validate its structure
-  //       expect(user.profile).toHaveProperty("id");
-  //       expect(user.profile).toHaveProperty("bio");
-  //       expect(user.profile).toHaveProperty("following");
-  //       expect(user.profile).toHaveProperty("followers");
-  //       expect(user.profile).toHaveProperty("createdAt");
-  //       expect(user.profile).toHaveProperty("updatedAt");
-  //       expect(user.profile).toHaveProperty("userId");
-  //     } else {
-  //       // Ensure the profile is explicitly null if no profile is associated
-  //       expect(user.profile).toBeNull();
-  //     }
-  //   });
-  // });
+      if (user.profile) {
+        // Check if the profile exists and validate its structure
+        expect(user.profile).toHaveProperty("id");
+        expect(user.profile).toHaveProperty("bio");
+        expect(user.profile).toHaveProperty("following");
+        expect(user.profile).toHaveProperty("followers");
+        expect(user.profile).toHaveProperty("createdAt");
+        expect(user.profile).toHaveProperty("updatedAt");
+        expect(user.profile).toHaveProperty("userId");
+      } else {
+        // Ensure the profile is explicitly null if no profile is associated
+        expect(user.profile).toBeNull();
+      }
+    });
+  });
 
   test("Create a new user", async () => {
     const user = {
