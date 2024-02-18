142c90f002c7d0c6912364c69941ed2a3a0534c6
Hubert He <hmh92@cornell.edu>
2024-02-18 02:53:38 +0000

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -5,11 +5,12 @@ import Button from "@/components/Button";
 const About = () => {
   return (
     <>
-      <div className="bg-yellow-300 backdrop-opacity-10 w-2/4 tracking-tighter">
-        Hello
-      </div>
-      <Button text="hello" />
-      Hello there
+      {" "}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="bg-red-500 p-4 rounded">red</div>
+        <div className="bg-blue-500 p-4 rounded">blue</div>
+        <div className="bg-green-500 p-4 sm:col-span-2 rounded">green</div>
+      </div>{" "}
     </>
   );
 };
