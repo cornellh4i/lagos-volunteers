891d152aeb1c4f1e9590f824f24db9f930c5c828
Diego Marques <dlm352@cornell.edu>
2024-02-18 07:07:35 +0000

    Tigers are excellent swimmers and do not avoid water.

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { useRouter } from "next/router";
+const getCatFact = async () => {
+  const response = await fetch("https://catfact.ninja/fact");
+  console.log(response.json());
+};
 
+const handleClick = () => {
+  getCatFact();
+};
 /** A User page */
 const User = () => {
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+  // return <>Hello user {userid}!</>;
+  <button onClick={handleClick}>cat fact</button>;
 };
 
 export default User;
