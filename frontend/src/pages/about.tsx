21851166aa6ea81f03d5720907ebcabdc8fc567b
Diego Marques <dlm352@cornell.edu>
2024-02-18 07:28:27 +0000

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -4,13 +4,18 @@ import Button from "@/components/button";
 /** An About page */
 const About = () => {
   return (
-    <>
-      <Button text="hello" />
-      {/* Modify the div with Tailwind CSS classes */}
-      <div className="bg-yellow-400 bg-opacity-10 w-1/2 text-opacity-50 text-center">
-        <p className="tracking-tighter">Hello</p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      {/* Box 1 */}
+      <div className="bg-red-500 rounded-lg p-4">Hello</div>
+      {/* Box 2 */}
+      <div className="bg-blue-500 rounded-lg p-4">Hello</div>
+      {/* Box 3 */}
+      <div className="bg-green-500 rounded-lg p-4 col-span-full sm:col-auto">
+        Hello
       </div>
-    </>
+      {/* Bottom Button */}
+      <Button text="Bottom Button" />
+    </div>
   );
 };
 
