abf68c874fa68fa15ac342eda226c4404a20adcd
Diego Marques <dlm352@cornell.edu>
2024-02-21 17:42:16 -0500

    onboard

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -5,15 +5,11 @@ import Button from "@/components/button";
 const About = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-      {/* Box 1 */}
       <div className="bg-red-500 rounded-lg p-4">Hello</div>
-      {/* Box 2 */}
       <div className="bg-blue-500 rounded-lg p-4">Hello</div>
-      {/* Box 3 */}
       <div className="bg-green-500 rounded-lg p-4 col-span-full sm:col-auto">
         Hello
       </div>
-      {/* Bottom Button */}
       <Button text="Bottom Button" />
     </div>
   );
