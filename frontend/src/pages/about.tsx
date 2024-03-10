92e8f028c2bed49bf2f64edbb16d49d9fd3641bd
Brandon Lerit <bsl77@cornell.edu>
2024-03-10 23:31:01 +0000

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -5,11 +5,11 @@ import Button from "@/components/Button";
 const About = () => {
   return (
     <>
-      <div className="bg-yellow-300 bg-opacity-10 w-1/2 tracking-tighter">
-        Hello
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+        <div className="bg-red-500 p-4 rounded">red</div>
+        <div className="bg-blue-500 p-4 rounded">blue</div>
+        <div className="bg-green-500 p-4 sm:col-span-2 rounded">green</div>
       </div>
-      <Button text="hello" />
-      Hello there
     </>
   );
 };
