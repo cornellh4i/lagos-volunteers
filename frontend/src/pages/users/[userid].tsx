9123a05e45e8295fcb9630ccfa2425b506d3e5fc
David <dev38@cornell.edu>
2024-03-10 17:35:36 +0000

    A cat will tremble or shiver when it is in extreme pain.

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -5,7 +5,20 @@ import { useRouter } from "next/router";
 const User = () => {
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+  return (
+    <>
+      Hello user {userid}!<button onClick={handleClick}>cat fact</button>
+    </>
+  );
+};
+
+const getCatFact = async () => {
+  const response = await fetch("https://catfact.ninja/fact");
+  console.log(response.json());
+};
+
+const handleClick = () => {
+  getCatFact();
 };
 
 export default User;
