2e9183e740bec38a2ba9335e49b4f814b8eb1cd5
tmavani23 <138815500+tmavani23@users.noreply.github.com>
2024-03-14 15:55:10 -0400

    Finished Jest onboarding

--- backend/tests/users.test.ts
+++ backend/tests/users.test.ts
@@ -29,6 +29,11 @@ describe("Testing user Endpoints", () => {
     const response = await request(app).post("/user/signup").send(user);
     expect(response.status).toBe(201);
   });
+
+  test("Get user", async () => {
+    const response = await request(app).get("/users");
+    expect(response.status).toBe(200);
+  });
 });
 
 // TODO: Add Tests for post endpoints
