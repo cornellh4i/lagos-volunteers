5cf8a3a05e362fffba0a2c2b56e9738a06496909
tmavani23 <138815500+tmavani23@users.noreply.github.com>
2024-03-14 16:46:26 -0400

    A cat almost never meows at another cat, mostly just humans. Cats typically will spit, purr, and hiss at other cats.

--- frontend/src/pages/users/[userid].tsx
+++ frontend/src/pages/users/[userid].tsx
@@ -5,7 +5,20 @@ import { useRouter } from "next/router";
 const User = () => {
   const router = useRouter();
   const { userid } = router.query;
-  return <>Hello user {userid}!</>;
+  return (
+    <div>
+      <>Hello user {userid}!</>;<button onClick={handleClick}>cat fact</button>
+    </div>
+  );
 };
+
+const getCatFact = async () => {
+    const response = await fetch("https://catfact.ninja/fact");
+    console.log(response.json());
+  };
+  
+  const handleClick = () => {
+    getCatFact();
+  };
 
 export default User;
