bca2d4fbd11a3607f8d624fbfa02919a9107f7d9
tmavani23 <138815500+tmavani23@users.noreply.github.com>
2024-03-14 21:41:49 -0400

    Complete responsive design onboarding

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -10,6 +10,11 @@ const About = () => {
       <div className="bg-yellow-300 bg-opacity-10 w-1/2 tracking-tighter">
         Hello
       </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2">
+        <div className="bg-red-500 p-4 rounded">red</div>
+        <div className="bg-blue-500 p-4 rounded">blue</div>
+        <div className="bg-green-500 p-4 sm:col-span-2 rounded">green</div>
+      </div>
     </>
   );
 };
