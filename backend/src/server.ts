8b5330953cb7ef71d0b49bbad0f043f635f09fa4
Owen Chen <owenchen502@gmail.com>
2024-03-23 15:06:43 -0400

    finished prune users function

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,12 +6,14 @@ import swaggerUI from "swagger-ui-express";
 import spec from "../api-spec.json";
 import cors from "cors";
 import cron from "node-cron";
+import { deleteUnverifiedUsers } from "./utils/helpers";
 
 const app: Application = express();
 
 // Scheduled cron jobs
-cron.schedule("*/1 * * * *", () => {
+cron.schedule("0 0 * * *", () => {
   console.log("running a task every minute");
+  deleteUnverifiedUsers();
 });
 
 // Middleware to parse json request bodies
--- backend/src/utils/helpers.ts
+++ backend/src/utils/helpers.ts
@@ -1,5 +1,26 @@
 import { Response } from "express";
 import { errorJson, successJson } from "./jsonResponses";
+import admin from "firebase-admin";
+import { User } from "@prisma/client";
+import deleteUser from "../users/controllers";
+import prisma from "../../client";
+
+// const serviceAccount = {
+//   type: process.env.TYPE,
+//   project_id: process.env.PROJECT_ID,
+//   private_key_id: process.env.PRIVATE_KEY_ID,
+//   private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+//   client_email: process.env.CLIENT_EMAIL,
+//   client_id: process.env.CLIENT_ID,
+//   auth_uri: process.env.AUTH_URI,
+//   token_uri: process.env.TOKEN_URI,
+//   auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_x509_CERT_URL,
+//   client_x509_cert_url: process.env.CLIENT_x509_CERT_URL,
+// };
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+// });
 
 /**
  * Attempts the given controller and sends a success code or error code as necessary
@@ -17,3 +38,48 @@ export const attempt = async (
     res.status(500).send(errorJson(error.message));
   }
 };
+
+interface UserRecord {
+  uid: string;
+  email?: string;
+  emailVerified?: boolean;
+  displayName?: string;
+  photoURL?: string;
+}
+
+/**
+ * Looks through Firebase and deletes every user created over 24 hours ago with an 
+ * unverified email. Also deletes the user from the local database.
+ */
+export const deleteUnverifiedUsers = async () => {
+  try {
+    const listUsersResult = await admin.auth().listUsers();
+    
+    const currentTime = Date.now();
+    const twentyFourHours = 24 * 60 * 60 * 1000;
+
+    for (const userRecord of listUsersResult.users) {
+      const userData = userRecord.toJSON()! as UserRecord;
+
+      const creationTime = new Date(userRecord.metadata.creationTime!).getTime();
+      const timeDifference = currentTime - creationTime;
+      
+      if (!userData.emailVerified && timeDifference > twentyFourHours) {
+        
+        // await admin.auth().deleteUser(userData.uid);
+        
+        // await prisma.user.delete({
+        //   where: {
+        //     email: userData.email,
+        //   },
+        // });
+        
+        console.log(`Deleted unverified user: ${userData.uid}`);
+      }
+    }
+    
+    console.log('Deletion complete.');
+  } catch (error) {
+    console.error('Error deleting unverified users:', error);
+  }
+}
