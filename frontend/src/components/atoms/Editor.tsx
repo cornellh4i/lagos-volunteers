ac44294a23854e9d9a52a693365f58244ab85eea
Jason Zheng <jasonz4200@gmail.com>
2024-03-23 23:49:07 -0400

    Add onChange support

--- frontend/src/components/atoms/Editor.tsx
+++ frontend/src/components/atoms/Editor.tsx
@@ -25,6 +25,7 @@ import {
 
 interface EditorProps {
   markdown: string;
+  onChange: (e: any) => void;
   editorRef?: React.MutableRefObject<MDXEditorMethods | null>;
 }
 
@@ -32,10 +33,11 @@ interface EditorProps {
  * Extend this Component further with the necessary plugins or props you need.
  * proxying the ref is necessary. Next.js dynamically imported components don't support refs.
  */
-const Editor: FC<EditorProps> = ({ markdown, editorRef }) => {
+const Editor: FC<EditorProps> = ({ markdown, onChange, editorRef }) => {
   return (
     <MDXEditor
       ref={editorRef}
+      onChange={onChange}
       markdown={markdown}
       plugins={[
         headingsPlugin(),
