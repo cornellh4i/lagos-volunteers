476fb6eb5488e021aa7963693e2e3c9d01bed1bd
Jason Zheng <jasonz4200@gmail.com>
2024-04-05 06:32:43 -0400

    Fix footer responsiveness

--- frontend/src/components/molecules/Footer.tsx
+++ frontend/src/components/molecules/Footer.tsx
@@ -5,11 +5,9 @@ const Footer = () => {
   return (
     <div className="border-solid border-0 border-t border-gray-300 ">
       <div className="max-w-screen-xl mx-auto p-6 sm:py-6">
-        <div className="flex justify-between">
-          <div className="text-left">
-            &copy; 2024 Hack4Impact Cornell. All rights reserved.
-          </div>
-          <div className="text-right">
+        <div className="flex flex-col gap-4 text-center sm:flex-row sm:justify-between sm:gap-0 sm:text-start">
+          <div>&copy; 2024 Hack4Impact Cornell</div>
+          <div>
             <Link className="text-black no-underline hover:underline" href="/">
               Terms of Service
             </Link>{" "}
