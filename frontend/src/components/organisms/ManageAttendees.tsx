fdaeac6ee0b1efe6fa15030e57f9584f3271cdfa
Arushi Aggarwal <aa2555@cornell.edu>
2024-04-10 21:45:06 +0000

    Implemented filtered search for users in ManageAttendees Co-authored-by: tmavani23 <tmavani23@users.noreply.github.com>

--- frontend/src/components/organisms/ManageAttendees.tsx
+++ frontend/src/components/organisms/ManageAttendees.tsx
@@ -145,17 +145,20 @@ const ManageAttendees = ({}: ManageAttendeesProps) => {
     if (cursor !== "") {
       if (searchQuery) {
         const { response, data } = await api.get(
+          //`/users?firstName=${searchQuery}`
           `/users?eventid=${eventid}&firstName=${searchQuery}`
         );
         return data;
       } else {
         const { response, data } = await api.get(
+          //`/users?limit=${paginationModel.pageSize}&after=${cursor}`
           `/users?eventid=${eventid}&limit=${paginationModel.pageSize}&after=${cursor}`
         );
         return data;
       }
     } else {
       const { response, data } = await api.get(
+        //`/users?limit=${paginationModel.pageSize}`
         `/users?eventid=${eventid}&limit=${paginationModel.pageSize}`
       );
       return data;
