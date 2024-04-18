4a2dbc80280e070f54c34d88d17d5f4e03fa55ff
Jason Zheng <jasonz4200@gmail.com>
2024-04-18 17:29:12 -0400

    Fix image not showing in edit event

--- frontend/src/components/atoms/Dropzone.tsx
+++ frontend/src/components/atoms/Dropzone.tsx
@@ -51,7 +51,20 @@ const Dropzone = ({
       <div className="flex items-center justify-center w-full">
         <label className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-50">
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
-            {selectedFile === null ? (
+            {props.defaultValue ? (
+              <div className="flex items-center">
+                <img src={props.defaultValue} className="max-h-20 mr-4" />
+                <div>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or
+                    drag and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    PNG or JPG (MAX. 50 MB)
+                  </p>
+                </div>
+              </div>
+            ) : selectedFile === null ? (
               <>
                 <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
                   <span className="font-semibold">Click to upload</span> or drag
