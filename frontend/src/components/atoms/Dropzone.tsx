77c2057d5e59ab3489d0e6995ab6a516cd7d1096
Jason Zheng <jasonz4200@gmail.com>
2024-04-18 16:52:20 -0400

    Revert dropzone

--- frontend/src/components/atoms/Dropzone.tsx
+++ frontend/src/components/atoms/Dropzone.tsx
@@ -53,18 +53,13 @@ const Dropzone = ({
           <div className="flex flex-col items-center justify-center pt-5 pb-6">
             {selectedFile === null ? (
               <>
-                <div className="flex items-center">
-                  <img src={props.defaultValue} className="max-h-20 mr-4" />
-                  <div>
-                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      <span className="font-semibold">Click to upload</span> or
-                      drag and drop
-                    </p>
-                    <p className="text-xs text-gray-500 dark:text-gray-400">
-                      PNG or JPG (MAX. 50 MB)
-                    </p>
-                  </div>
-                </div>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span> or drag
+                  and drop
+                </p>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  PNG or JPG (MAX. 50 MB)
+                </p>
               </>
             ) : (
               <>
