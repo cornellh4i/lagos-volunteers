65e795b9d38243657ccb85b684fb976c4548c59c
Owen Chen <ojc7@cornell.edu>
2024-04-24 17:19:08 -0400

    simplify /users/googleCreate endpoint logic

--- backend/src/users/views.ts
+++ backend/src/users/views.ts
@@ -81,36 +81,22 @@ userRouter.post(
   async (req: Request, res: Response) => {
     // #swagger.tags = ['Users']
     socketNotify("/users");
-    let user;
     const { ...rest } = req.body;
+    
     try {
-      // Create user in local database
-      user = await userController.createUser(
+      await firebase.auth().setCustomUserClaims(rest.id, {
+        admin: false,
+        supervisor: false,
+        volunteer: true,
+      });
+  
+      attempt(res, 200, () => userController.createUser(
         rest,
         rest.profile,
         rest.preferences,
         rest.permissions
-      );
-
-      // If local user doesn't exist, throw error
-      if (!user) {
-        throw Error("Failed to create local user");
-      }
-
-      // If test environment, return user without firebase auth
-      else if (process.env.NODE_ENV === "test") {
-        return res.status(201).send({ success: true, data: user });
-      }
-      
+      ));
     } catch (e: any) {
-      // If user exists in local database but not in Firebase, delete user
-      if (user) {
-        try {
-          await userController.deleteUser(user.id);
-        } catch (e: any) {
-          return res.status(500).send({ success: false, error: e.message });
-        }
-      }
       return res.status(500).send({ success: false, error: e.message });
     }
   }
