e43c76335a1f4d05c75c44a136a17aacf95a7d98
Jason Zheng <48730262+jasozh@users.noreply.github.com>
2025-01-02 19:26:48 -0500

    Fix overflow clip of image in ViewEventDetails (#284)

--- frontend/src/components/templates/EventTemplate.tsx
+++ frontend/src/components/templates/EventTemplate.tsx
@@ -26,8 +26,8 @@ const EventTemplate = ({ header, body, img, card }: EventTemplateProps) => {
 
         {/* Right column */}
         <div className="w-96">
-          <div className="max-h-96 overflow-hidden">{img}</div>
-          <div className="mt-6 sticky top-5">{card}</div>
+          <div>{img}</div>
+          <div className="mt-8 sticky">{card}</div>
         </div>
       </div>
 
