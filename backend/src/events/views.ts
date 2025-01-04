c257ec710010f63c10c1796517fc10c2e4385c2d
Jason Zheng <jasonz4200@gmail.com>
2025-01-04 17:44:41 -0600

    Strengthen backend route protections

--- backend/src/events/views.ts
+++ backend/src/events/views.ts
@@ -7,7 +7,11 @@ import {
   authIfSupervisorOrAdmin,
   authIfVolunteer,
 } from "../middleware/auth";
-import { attempt, socketNotify } from "../utils/helpers";
+import {
+  attempt,
+  checkUserMatchOrSupervisorAdmin,
+  socketNotify,
+} from "../utils/helpers";
 
 import { errorJson, successJson } from "../utils/jsonResponses";
 import { EventMode, EventStatus, Prisma } from "@prisma/client";
@@ -161,11 +165,13 @@ eventRouter.post(
   useAuth,
   async (req: Request, res: Response) => {
     // #swagger.tags = ['Events']
-    const { attendeeid } = req.body;
-    attempt(res, 200, () =>
-      eventController.addAttendee(req.params.eventid, attendeeid)
-    );
-    socketNotify(`/events/${req.params.eventid}`);
+    checkUserMatchOrSupervisorAdmin(req, res, req.params.userid, async () => {
+      const { attendeeid } = req.body;
+      attempt(res, 200, () =>
+        eventController.addAttendee(req.params.eventid, attendeeid)
+      );
+      socketNotify(`/events/${req.params.eventid}`);
+    });
   }
 );
 
@@ -191,15 +197,17 @@ eventRouter.put(
   useAuth,
   async (req: Request, res: Response) => {
     // #swagger.tags = ['Events']
-    const { cancelationMessage } = req.body;
-    attempt(res, 200, () =>
-      eventController.deleteAttendee(
-        req.params.eventid,
-        req.params.attendeeid,
-        cancelationMessage
-      )
-    );
-    socketNotify(`/events/${req.params.eventid}`);
+    checkUserMatchOrSupervisorAdmin(req, res, req.params.userid, async () => {
+      const { cancelationMessage } = req.body;
+      attempt(res, 200, () =>
+        eventController.deleteAttendee(
+          req.params.eventid,
+          req.params.attendeeid,
+          cancelationMessage
+        )
+      );
+      socketNotify(`/events/${req.params.eventid}`);
+    });
   }
 );
 
