313853e5b548f77dd452284401655eadb9944fd8
Jason Zheng <jasonz4200@gmail.com>
2025-04-22 22:53:29 -0400

    Fix cancel confirmation

--- frontend/src/components/organisms/EventCardCancelConfirmation.tsx
+++ frontend/src/components/organisms/EventCardCancelConfirmation.tsx
@@ -7,12 +7,9 @@ const EventCardCancelConfirmation = ({}: EventCardCancelConfirmationProps) => {
   return (
     <Card>
       <div className="font-semibold text-2xl">You are no longer registered</div>
-      <div className="mt-5" />
-      <div className="font-semibold">
+      <div className="mt-5">
         We're sorry you can't make it! Thank you for letting us know.
       </div>
-      <div className="mt-2" />
-      <div>Explore other volunteer opportunities on the home page.</div>
     </Card>
   );
 };
